1974c6d071d44bbf56cea64d4bf38eb6b63026ea
Usama Saleem <usama.saleem@hotmail.com>
2023-09-25 15:45:15 -0400

    Added navbar template

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -1,31 +1,106 @@
-// components/Navbar.js
-import { Box, Flex, Heading, Spacer, VStack, Link } from "@chakra-ui/react";
-import NextLink from "next/link"; // Import Next.js Link component
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Text,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
-export default function Navbar() {
+const Links = ["Dashboard", "Projects", "Team"];
+
+const NavLink = (props) => {
+  const { children } = props;
+
   return (
     <Box
-      bg="rgba(255, 255, 255, 0.5)"
-      backdropFilter="blur(10px)"
-      py={4}
-      px={6}
-      pos="fixed"
-      w="100%"
-      zIndex={999}
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={"#"}
     >
-      <Flex maxW="container.xl" mx="auto" alignItems="center">
-        <Heading size="lg">My Website</Heading>
-        <Spacer />
-        <VStack spacing={2}>
-          <NextLink href="/" passHref>
-            <Link>Home</Link>
-          </NextLink>
-          <NextLink href="/about" passHref>
-            <Link>About</Link>
-          </NextLink>
-          {/* Add more navigation links here */}
-        </VStack>
-      </Flex>
+      {children}
     </Box>
+  );
+};
+
+export default function navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>Logo</Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+                minW={0}
+              >
+                <Avatar
+                  size={"sm"}
+                  src={
+                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                  }
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Link 1</MenuItem>
+                <MenuItem>Link 2</MenuItem>
+                <MenuDivider />
+                <MenuItem>Link 3</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
   );
 }
