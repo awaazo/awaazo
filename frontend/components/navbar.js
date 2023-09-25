f47a11506b6ec0e1f832593d6352952646d17347
Usama Saleem <usama.saleem@hotmail.com>
2023-09-25 15:04:38 -0400

    Frontend setup with Chakra UI

---
+++ frontend/components/navbar.js
@@ -0,0 +1,31 @@
+// components/Navbar.js
+import { Box, Flex, Heading, Spacer, VStack, Link } from "@chakra-ui/react";
+import NextLink from "next/link"; // Import Next.js Link component
+
+export default function Navbar() {
+  return (
+    <Box
+      bg="rgba(255, 255, 255, 0.5)" // Semi-transparent white background for frosted effect
+      backdropFilter="blur(10px)" // Apply blur effect
+      py={4}
+      px={6}
+      pos="fixed"
+      w="100%"
+      zIndex={999}
+    >
+      <Flex maxW="container.xl" mx="auto" alignItems="center">
+        <Heading size="lg">My Website</Heading>
+        <Spacer />
+        <VStack spacing={2} alignItems="flex-end">
+          <NextLink href="/" passHref>
+            <Link>Home</Link>
+          </NextLink>
+          <NextLink href="/about" passHref>
+            <Link>About</Link>
+          </NextLink>
+          {/* Add more navigation links here */}
+        </VStack>
+      </Flex>
+    </Box>
+  );
+}
