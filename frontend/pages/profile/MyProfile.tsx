0d76caaf9b1887af5e15606369816251b7cd03d7
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2023-10-05 17:54:35 -0400

    Update MyProfile.tsx

--- frontend/pages/profile/MyProfile.tsx
+++ frontend/pages/profile/MyProfile.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import {
   Container,
   Center,
+  VStack,
+  Box,
+  HStack,
 } from '@chakra-ui/react';
 
 import Navbar from "../../components/navbar"; // Import the Navbar component
@@ -13,14 +16,20 @@ const myProfile = () => {
   return (
     <>
     <Navbar />
+    <HStack>
+    <VStack>
+
     <Container paddingTop={"10em"}>
-
         <Header/> 
-
     </Container>
     <Container p={{ base: 5, md: 10 }}>
       <MyEpisodes/>
     </Container>
+    </VStack>
+    <Box>
+    </Box>
+
+    </HStack>
     </>
   );
 };
