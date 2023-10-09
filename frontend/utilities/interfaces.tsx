16cd3bfb22c3a20204ccfb634a87507cc037babf
jonathan <106629548+jonathan-githubofficial@users.noreply.github.com>
2023-10-09 16:46:01 -0400

    removed comments

--- frontend/utilities/interfaces.tsx
+++ frontend/utilities/interfaces.tsx
@@ -1,115 +1,103 @@
-// Podcast Interface
 export interface Podcast {
-  id: string; // Unique identifier for the podcast
-  coverArt: string; // URL to the cover art image of the podcast
-  podcasterId: string; // ID of the podcaster/creator
-  tags: string[]; // Array of tags/categories associated with the podcast
-  isExplicit?: boolean; // Optional flag indicating if the podcast has explicit content
-  type: "real" | "ai-generated"; // Type of podcast: either real or AI-generated
-  creationDate: Date; // Date when the podcast was created
-  episodes: Episode[]; // Array of episodes belonging to this podcast
-  averageRating?: number; // Optional computed average of all ratings for this podcast
-  totalRatings?: number; // Optional total number of ratings received for this podcast
-}
-
-// Episode Interface
-export interface Episode {
-  id: string; // Unique identifier for the episode
-  podcastId: string; // ID of the podcast this episode belongs to
-  episodeName: string; // Name of the episode
-  thumbnail: string; // URL to the thumbnail image of the episode
-  duration: number; // Duration of the episode in seconds
-  releaseDate: Date; // Date when the episode was released
-  isExplicit?: boolean; // Optional flag indicating if the episode has explicit content
-  playCount?: number; // Optional count of times the episode has been played
-  likes: {
-    count: number; // Number of likes the episode has received
-    isLiked: boolean; // Flag indicating if the user has liked the episode
-  };
-  comments: {
-    count: number; // Number of comments the episode has received
-    isCommented: boolean; // Flag indicating if the user has commented on the episode
-  };
-  bookmarks?: Bookmark[]; // Optional array of bookmarks added to the episode by the user
-  sections?: {
-    startTime: number; // Start time of the section within the episode
-    sectionName: string; // Name of the section
-  }[];
-  annotations: Annotation[]; // Updated to include the extended Annotation interface
-  sponsors: Sponsor[]; // Array of sponsors for this episode
-}
-
-// Annotation Interface
-export interface Annotation {
-  timestamp: number; // Timestamp of the annotation within the episode
-  content: string; // Content of the annotation
-  type: "link" | "info" | "sponsorship" | "media-link";
-  sponsorship?: Sponsor; // Optional sponsorship details, present if type is 'sponsorship'
-  mediaLink?: MediaLink; // Optional media lin details, present if type is 'media-link'
-}
-
-// Sponsor Interface
-export interface Sponsor {
-    id: string; // Unique identifier for the sponsor
-    name: string; // Name of the sponsor
-    website: string; // URL to the sponsor's website
+    id: string;
+    coverArt: string;
+    podcasterId: string;
+    tags: string[];
+    isExplicit?: boolean;
+    type: "real" | "ai-generated";
+    creationDate: Date;
+    episodes: Episode[];
+    averageRating?: number;
+    totalRatings?: number;
   }
   
-  // Media Link Interface
-  export interface MediaLink {
-    platform: "YouTube" | "Spotify" | "Apple Music"; // Platform where the media is hosted
-    url: string; // URL to the media
+  export interface Episode {
+    id: string;
+    podcastId: string;
+    episodeName: string;
+    thumbnail: string;
+    duration: number;
+    releaseDate: Date;
+    isExplicit?: boolean;
+    playCount?: number;
+    likes: {
+      count: number;
+      isLiked: boolean;
+    };
+    comments: {
+      count: number;
+      isCommented: boolean;
+    };
+    bookmarks?: Bookmark[];
+    sections?: {
+      startTime: number;
+      sectionName: string;
+    }[];
+    annotations: Annotation[];
+    sponsors: Sponsor[];
   }
-
-// Bookmark Interface
-export interface Bookmark {
-  title: string; // Title of the bookmark
-  note?: string; // Optional note associated with the bookmark
-  time: number; // Time of the bookmark within the episode
-}
-
-// User Interface
-export interface User {
-  id: string; // Unique identifier for the user
-  username: string; // Username of the user
-  email: string; // Email address of the user
-  passwordHash: string; // Hashed password of the user
-  salt: string; // Salt used for hashing the password
-  avatar: string; // URL to the user's avatar image
-  interests: string[]; // Array of interests of the user
-  dateOfBirth: Date; // Date of birth of the user
-  gender: "male" | "female" | "other" | "prefer not to say"; // Gender of the user
-  isPodcaster: boolean; // Flag indicating if the user is a podcaster
-  podcasts?: Podcast[]; // Optional array of podcasts created by the user
-}
-
-// Podcast Follow Interface
-export interface PodcastFollow {
-  userId: string; // ID of the user following the podcast
-  podcastId: string; // ID of the podcast being followed
-}
-
-// User Follow Interface
-export interface UserFollow {
-  followerId: string; // ID of the user following another user
-  followingId: string; // ID of the user being followed
-}
-
-// Subscription Interface
-export interface Subscription {
-  userId: string; // ID of the subscribed user
-  type: "basic" | "premium"; // Type of subscription: either basic or premium
-  startDate: Date; // Start date of the subscription
-  endDate: Date; // End date of the subscription
-}
-
-// Podcast Rating Interface
-export interface PodcastRating {
-  id: string; // Unique identifier for each rating entry
-  userId: string; // ID of the user who provided the rating
-  podcastId: string; // ID of the podcast being rated
-  rating: number; // The rating value, e.g., from 1 to 5
-  timestamp: Date; // When the rating was provided
-}
-
-
+  
+  export interface Annotation {
+    timestamp: number;
+    content: string;
+    type: "link" | "info" | "sponsorship" | "media-link";
+    sponsorship?: Sponsor;
+    mediaLink?: MediaLink;
+  }
+  
+  export interface Sponsor {
+    id: string;
+    name: string;
+    website: string;
+  }
+  
+  export interface MediaLink {
+    platform: "YouTube" | "Spotify" | "Apple Music";
+    url: string;
+  }
+  
+  export interface Bookmark {
+    title: string;
+    note?: string;
+    time: number;
+  }
+  
+  export interface User {
+    id: string;
+    username: string;
+    email: string;
+    passwordHash: string;
+    salt: string;
+    avatar: string;
+    interests: string[];
+    dateOfBirth: Date;
+    gender: "male" | "female" | "other" | "prefer not to say";
+    isPodcaster: boolean;
+    podcasts?: Podcast[];
+  }
+  
+  export interface PodcastFollow {
+    userId: string;
+    podcastId: string;
+  }
+  
+  export interface UserFollow {
+    followerId: string;
+    followingId: string;
+  }
+  
+  export interface Subscription {
+    userId: string;
+    type: "basic" | "premium";
+    startDate: Date;
+    endDate: Date;
+  }
+  
+  export interface PodcastRating {
+    id: string;
+    userId: string;
+    podcastId: string;
+    rating: number;
+    timestamp: Date;
+  }
+  
