ed0184b7bd85433c6be5854d5e1cece65443302f
Mathieu Trudeau <mathieutrudeau11@gmail.com>
2023-10-12 14:19:33 -0400

    Revert "Revert "Merge branch 'feature/signup' of https://github.com/jonathan-githubofficial/awaazo into feature/signup""
    This reverts commit 560a2f2d998a22da645202545d6fe093f23c044e.

--- Backend/Backend/Migrations/20230925225241_InitialMigration.cs
+++ Backend/Backend/Migrations/20230925225241_InitialMigration.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace Backend.Migrations
 {
+    [ExcludeFromCodeCoverage]
     public partial class InitialMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
---
+++ Backend/Backend/Models/Annotation.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models;
+
+public class Annotation : BaseEntity
+{
+    public Annotation() 
+    {
+        Content = string.Empty;
+    }
+
+    [Key]
+    public Guid Id { get; set; }
+    
+    public Episode Episode { get; set; } = null!;
+    public Guid EpisodeId { get; set; }
+    
+    public double Timestamp { get; set; }
+    public string Content { get; set; }
+    public AnnotationType Type { get; set;}
+    
+    /// <summary>
+    /// Optional sponsorship details, present if type is 'sponsorship'
+    /// </summary>
+    public Sponsor? Sponsorship { get; set; } = null;
+    
+    /// <summary>
+    ///  Optional media lin details, present if type is 'media-link'
+    /// </summary>
+    public MediaLink? MediaLink { get; set; }
+    
+    public enum AnnotationType
+    {
+        Link, Info, Sponsorship, MediaLink
+    }
+}
