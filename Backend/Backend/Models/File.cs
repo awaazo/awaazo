943db57e9e8c1db11a651af98510a7c102f05e47
Muhammad Mubashir <mubashirqbl@gmail.com>
2023-10-21 20:45:14 -0400

    Added File Model

---
+++ Backend/Backend/Models/File.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class File : BaseEntity
+    {
+
+        public File() {
+
+            Name = string.Empty;
+            MimeType = string.Empty;
+        
+
+        }
+
+        public Guid FileId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string MimeType { get; set; }
+
+        [Required]
+        public float Size { get; set; } = 0;
+
+        [Required]
+        public Byte[] Data { get; set; } = Array.Empty<Byte>();
+
+
+    }
+}
