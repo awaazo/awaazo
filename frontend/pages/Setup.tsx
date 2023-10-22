5da89580e8dd6be0f5ada18105d365a584956ab9
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2023-10-22 18:28:52 -0400

    Adjusted styling for setup page

--- frontend/pages/Setup.tsx
+++ frontend/pages/Setup.tsx
@@ -8,6 +8,8 @@ import {
   Input,
   Stack,
   Text,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
@@ -51,7 +53,7 @@ const Setup: React.FC = () => {
 
   useEffect(() => {
     // Check to make sure the user has logged in
-    if (!isLoggedIn) {
+    if (isLoggedIn) {
       window.location.href = loginPage;
     }
   }, [session, router]);
@@ -66,14 +68,20 @@ const Setup: React.FC = () => {
     //window.location.href = mainPage;
   };
 
-  function getRandomColor() {
+  function getRandomDarkColor() {
     const letters = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 8)]; // Restrict to the first 8 characters for darker colors
     }
     return color;
-  }
+}
+
+function getRandomGradient() {
+    const color1 = getRandomDarkColor();
+    const color2 = getRandomDarkColor();
+    return `linear-gradient(45deg, ${color1}, ${color2})`;
+}
 
   const handleInterestClick = (genre) => {
     if (selectedInterests.includes(genre)) {
@@ -81,7 +89,7 @@ const Setup: React.FC = () => {
     } else {
       setSelectedInterests([...selectedInterests, genre]);
       if (!genreColors[genre]) {
-        setGenreColors({ ...genreColors, [genre]: getRandomColor() });
+        setGenreColors({ ...genreColors, [genre]: getRandomDarkColor() });
       }
     }
   };
@@ -100,12 +108,12 @@ const Setup: React.FC = () => {
           alt="logo"
           style={{
             maxHeight: "10em",
-            maxWidth: "20em",
+            maxWidth: "3em",
           }}
         />
         <Text
           style={{
-            fontSize: "2rem",
+            fontSize: "1.5rem",
             fontWeight: "bold",
             textAlign: "center",
             marginBottom: "1rem",
@@ -114,40 +122,40 @@ const Setup: React.FC = () => {
           Finish Setting up Your Account
         </Text>
 
-        <form onSubmit={handleSetup}>
-          <Stack spacing={4}>
+         <form onSubmit={handleSetup}>
+          <Stack spacing={6} align={"center"}>
             <div
               style={{
+                position: "relative",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
               }}
             >
-              <FormLabel>Avatar (Optional)</FormLabel>
               <img
                 src={
                   avatar ||
-                  "https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-                } // Display uploaded avatar or default image
+                  "https://img.icons8.com/?size=512&id=492ILERveW8G&format=png"
+                }
                 alt="Avatar"
                 style={{
                   width: "150px",
                   height: "150px",
                   borderRadius: "50%",
                   padding: "15px",
+                  position: "relative",
                 }}
               />
               <Button
                 colorScheme="gray"
                 style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  padding: "10px",
-                  borderRadius: "5px",
+                  position: "absolute",
+                  bottom: "5px",
+                  right: "5px",
+                  padding: "5px",
+                  borderRadius: "10px",
                   cursor: "pointer",
-                  marginBottom: "10px",
-                  width: "90%",
+                  fontSize: "0.8rem", // Smaller font size
                 }}
               >
                 <label
@@ -156,7 +164,7 @@ const Setup: React.FC = () => {
                     cursor: "pointer",
                   }}
                 >
-                  Upload Avatar
+                  Upload
                 </label>
                 <input
                   type="file"
@@ -173,63 +181,73 @@ const Setup: React.FC = () => {
             </div>
 
             <FormControl>
-              <FormLabel>Bio (Optional)</FormLabel>
               <Textarea
                 id="bio"
-                placeholder="Enter your bio"
+                placeholder="What's your story? (Optional)"
                 value={bio}
                 onChange={(e) => setBio(e.target.value)}
                 style={{
                   width: "100%",
-                  height: "150px",
+                  height: "100px",
                   padding: "12px",
                   fontSize: "16px",
                 }}
-                resize="none"
+                resize="vertical" // Made the bio textarea resizable
               />
             </FormControl>
 
-            <FormControl>
-              <FormLabel>Interests (Optional)</FormLabel>
-              <div
-                style={{
-                  padding: "10px",
-                  border: "1px solid #445670",
-                  borderRadius: "4px",
-                  gap: "10px",
-                  maxWidth: "300px",
-                }}
-              >
-                {PodcastGenres.map((genre) => (
-                  <Button
-                    key={genre}
-                    style={{
-                      padding: "10px",
-                      fontSize: "15px",
-                      cursor: "pointer",
-                      backgroundColor: selectedInterests.includes(genre)
-                        ? genreColors[genre] || getRandomColor()
-                        : "gray",
-                      border: "1px solid #000",
-                      borderRadius: "4px",
-                      width: "fit-content",
-                    }}
-                    onClick={() => handleInterestClick(genre)}
-                  >
-                    {genre}
-                  </Button>
-                ))}
-              </div>
-            </FormControl>
+             <FormControl>
+              <FormLabel style={
+                {
+                   textAlign: "center",
+                   padding: "10px",
+                  }
+                   }>
+                What kind of topics do you like?
+              </FormLabel>
+              <Wrap spacing={4} justify="center" maxWidth={"600px"}>
+    {PodcastGenres.map((genre) => (
+      <WrapItem key={genre}>
+        <Button
+          size="sm"
+          variant={selectedInterests.includes(genre) ? "solid" : "outline"}
+          colorScheme="white"
+          backgroundColor={
+            selectedInterests.includes(genre)
+              ? genreColors[genre] || getRandomGradient()
+              : "transparent"
+          }
+          color="white"
+          borderColor="white"
+          borderRadius="full"
+          _hover={{
+            backgroundColor: selectedInterests.includes(genre)
+              ? genreColors[genre] || getRandomGradient()
+              : "gray",
+          }}
+          onClick={() => handleInterestClick(genre)}
+        >
+          {genre}
+        </Button>
+      </WrapItem>
+    ))}
+  </Wrap>
+</FormControl>
 
-            <Button
+
+
+              <Button
               id="setupBtn"
               type="submit"
-              colorScheme="gray"
-              size="lg"
               fontSize="md"
+              borderRadius={"full"}
+              backgroundColor="white"
+              maxWidth={"200px"}
+              color={"black"}
+              marginTop={"15px"}
+              marginBottom={"10px"}
             >
-              Finsh Account Setup
+              Finish Account Setup
             </Button>
           </Stack>
         </form>
