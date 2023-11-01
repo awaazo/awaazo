2db821c16495584778a0f1b4aaca0f3d81250963
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2023-11-01 17:38:21 -0400

    Update Navbar.tsx

--- frontend/components/shared/Navbar.tsx
+++ frontend/components/shared/Navbar.tsx
@@ -131,8 +131,7 @@ export default function Navbar() {
         borderRadius={"95px"}
         boxShadow="0px 0px 15px rgba(0, 0, 0, 0.4)"
         data-testid="navbar-component"
-        // semi transparent white outline
-        border="3px solid rgba(255, 255, 255, 0.05)"
+        border={colorMode === "dark" ? "3px solid rgba(255, 255, 255, 0.05)" : "3px solid rgba(0, 0, 0, 0.05)"}
       >
         <Flex alignItems={"center"} justifyContent={"space-between"} px={6}>
           <Link href="/Main">
@@ -142,7 +141,9 @@ export default function Navbar() {
           </Link>
           {isMobile ? (
             <Flex alignItems={"center"}>
-              <IconButton aria-label="Search" icon={<SearchIcon />} onClick={handleSearchSubmit} variant="ghost" size="md" rounded={"full"} opacity={0.7} mr={4} />
+              <IconButton aria-label="Search" icon={<SearchIcon />} onClick={handleSearchSubmit} variant="ghost" size="md" rounded={"full"} opacity={0.9} mr={4} 
+              color={colorMode === "dark" ? "white" : "black"}
+              />
               <UserProfileMenu />
             </Flex>
           ) : (
@@ -153,10 +154,17 @@ export default function Navbar() {
                 e.preventDefault();
                 handleSearchSubmit();
               }}
+              color={colorMode === "dark" ? "white" : "black"}
             >
-              <Input placeholder="Search" size="sm" borderRadius="full" mr={4} value={searchValue} onChange={handleSearchChange} />
+              <Input placeholder="Search" size="sm" borderRadius="full" mr={4} value={searchValue} onChange={handleSearchChange} 
+              color={colorMode === "dark" ? "white" : "black"}
+              />
               <Link href="/Create">
-                <IconButton aria-label="Create" icon={<AddIcon />} variant="ghost" size="md" rounded={"full"} opacity={0.7} mr={4} color={colorMode === "dark" ? "white" : "black"} />
+                <Button colorScheme="teal" variant="ghost" size="md" rounded={"full"} opacity={0.9} mr={4} color={colorMode === "dark" ? "white" : "black"} 
+                  fontFamily={'Avenir Next'}
+                >
+                  Upload
+                </Button>
               </Link>
               {isUserLoggedIn ? <UserProfileMenu /> : <LoggedOutMenu />}
             </Flex>
