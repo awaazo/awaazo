4b39fe3257f587d8f8feb0198070745edf6bdf7f
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2023-11-02 18:58:48 -0400

    Added new nav bar and player bar

--- frontend/components/shared/Navbar.tsx
+++ frontend/components/shared/Navbar.tsx
@@ -2,14 +2,37 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { Profile } from "next-auth";
-import { Box, Flex, Avatar, IconButton, Button, Menu, MenuButton, MenuList, MenuItem, MenuDivider, MenuGroup, useColorModeValue, useColorMode, Image, Input, useBreakpointValue, Icon } from "@chakra-ui/react";
-import { MoonIcon, SunIcon, SearchIcon, AddIcon, HamburgerIcon, BellIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Avatar,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  MenuGroup,
+  useColorModeValue,
+  useColorMode,
+  Image,
+  Input,
+  useBreakpointValue,
+  Icon,
+} from "@chakra-ui/react";
+import {
+  MoonIcon,
+  SunIcon,
+  SearchIcon,
+  AddIcon,
+  HamburgerIcon,
+} from "@chakra-ui/icons";
 import LogoWhite from "../../public/logo_white.svg";
 import LogoBlack from "../../public/logo_black.svg";
 import AuthHelper from "../../helpers/AuthHelper";
 import UserProfileHelper from "../../helpers/UserProfileHelper";
 import { UserMenuInfo } from "../../utilities/Interfaces";
-import Notifications from "../../pages/notification/Notifications";
 
 export default function Navbar() {
   const loginPage = "/auth/Login";
@@ -29,33 +52,33 @@ export default function Navbar() {
   });
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false); // New state to track login status
   const [isUserSet, setIsUserSet] = useState(false);
-  const [isNotificationsOpen, setIsNotificationsOpen] = useState(false);
-
-  const toggleNotifications = () => {
-  setIsNotificationsOpen(!isNotificationsOpen);
-};
 
   useEffect(() => {
     // Custom User logged in
     if (!isUserSet) {
       AuthHelper.authMeRequest().then((response) => {
-        if(response.status == 200){
-        setUser(response.userMenuInfo);
-        setIsUserLoggedIn(true); // Set login status to true
-        setIsUserSet(true);
-        setIsLoggedIn(true)
+        if (response.status == 200) {
+          setUser(response.userMenuInfo);
+          setIsUserLoggedIn(true); // Set login status to true
+          setIsUserSet(true);
+          setIsLoggedIn(true);
         }
       });
     }
     // Google User logged in
     else if (session && !isLoggedIn) {
-      AuthHelper.loginGoogleSSO(session.user.email, session.user.name, (session as any).id, session.user.image).then(() => {
+      AuthHelper.loginGoogleSSO(
+        session.user.email,
+        session.user.name,
+        (session as any).id,
+        session.user.image
+      ).then(() => {
         AuthHelper.authMeRequest().then((response) => {
-          if(response.status == 200){
-          setUser(response.userMenuInfo);
-          setIsUserLoggedIn(true); // Set login status to true
-          setIsUserSet(true);
-          setIsLoggedIn(true)
+          if (response.status == 200) {
+            setUser(response.userMenuInfo);
+            setIsUserLoggedIn(true); // Set login status to true
+            setIsUserSet(true);
+            setIsLoggedIn(true);
           }
         });
       });
@@ -78,28 +101,55 @@ export default function Navbar() {
 
   const UserProfileMenu = () => (
     <Menu>
-      <MenuButton aria-label="loggedInMenu" as={Button} rounded={"full"} variant={"link"} cursor={"pointer"}>
-        {user.avatarUrl === "" ? 
-        <Avatar size={"sm"} src={"https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"} boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)" bg="rgba(255, 255, 255, 0.2)" backdropFilter="blur(10px)" /> : 
-        <Avatar size={"sm"} src={user.avatarUrl} boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)" bg="rgba(255, 255, 255, 0.2)" backdropFilter="blur(10px)" />}
+      <MenuButton
+        aria-label="loggedInMenu"
+        as={Button}
+        rounded={"full"}
+        variant={"link"}
+        cursor={"pointer"}
+      >
+        {user.avatarUrl === "" ? (
+          <Avatar
+            size={"sm"}
+            src={
+              "https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+            }
+            boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)"
+          />
+        ) : (
+          <Avatar
+            size={"sm"}
+            src={user.avatarUrl}
+            boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)"
+            bg="rgba(255, 255, 255, 0.2)"
+            backdropFilter="blur(10px)"
+          />
+        )}
       </MenuButton>
       <MenuList>
-        <MenuItem onClick={toggleColorMode}>{colorMode === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}</MenuItem>
-        <MenuDivider />
         <MenuGroup>
-          <MenuItem>{user.username}</MenuItem>
-          <MenuDivider />
-          <MenuItem onClick={() => (window.location.href = "/profile/MyProfile")}>My Account</MenuItem>
-          <MenuItem onClick={() => (window.location.href = "/profile/MyPodcast")}>My Podcast</MenuItem>
-          <MenuItem>Payments</MenuItem>
-        </MenuGroup>
-        <MenuDivider />
-        <MenuGroup title="Help">
-          <MenuItem>Docs</MenuItem>
-          <MenuItem>FAQ</MenuItem>
+          <MenuItem
+            onClick={() => (window.location.href = "/profile/MyProfile")}
+          >
+            👤 My Account
+          </MenuItem>
+          <MenuItem
+            onClick={() => (window.location.href = "/profile/MyPodcasts")}
+          >
+            🎙️ My Podcasts
+          </MenuItem>
           <MenuDivider />
 
-          <MenuItem onClick={handleLogOut} style={{ color: "red", fontWeight: "bold" }}>
+          <MenuItem onClick={() => (window.location.href = "/Settings")}>
+            ⚙️ Settings
+          </MenuItem>
+        </MenuGroup>
+        <MenuDivider />
+        <MenuGroup>
+          <MenuItem
+            onClick={handleLogOut}
+            style={{ color: "red", fontWeight: "normal" }}
+          >
             Logout
           </MenuItem>
         </MenuGroup>
@@ -109,24 +159,29 @@ export default function Navbar() {
 
   const LoggedOutMenu = () => (
     <Menu>
-      <MenuButton menu-id="menuBtn" aria-label="Menu" as={Button} variant={"link"} cursor={"pointer"}>
+      <MenuButton
+        menu-id="menuBtn"
+        aria-label="Menu"
+        as={Button}
+        variant={"link"}
+        cursor={"pointer"}
+      >
         <HamburgerIcon />
       </MenuButton>
       <MenuList>
-        <MenuItem id="loginBtn" onClick={() => (window.location.href = loginPage)}>
+        <MenuItem
+          id="loginBtn"
+          onClick={() => (window.location.href = loginPage)}
+        >
           Login
         </MenuItem>
         <MenuDivider />
-        <MenuItem onClick={() => (window.location.href = registerPage)}>Register</MenuItem>
+        <MenuItem onClick={() => (window.location.href = registerPage)}>
+          Register
+        </MenuItem>
       </MenuList>
     </Menu>
   );
-const NotificationsModal = () => {
-  return (
-
-         <Notifications isOpen={isNotificationsOpen} onClose={toggleNotifications} />
-  );
-};
 
   return (
     <>
@@ -139,22 +194,47 @@ const NotificationsModal = () => {
         mb={"3em"}
         position="sticky"
         top={5}
-        zIndex={1001}
+        zIndex={999}
         borderRadius={"95px"}
         boxShadow="0px 0px 15px rgba(0, 0, 0, 0.4)"
         data-testid="navbar-component"
-        // semi transparent white outline
         border="3px solid rgba(255, 255, 255, 0.05)"
       >
         <Flex alignItems={"center"} justifyContent={"space-between"} px={6}>
-          <Link href="/Main">
+          <Link href="/">
             <Box maxWidth={"1.5em"} ml={-2}>
-              <Image src={colorMode === "dark" ? LogoWhite.src : LogoBlack.src} alt="logo" />
+              <Image
+                src={colorMode === "dark" ? LogoWhite.src : LogoBlack.src}
+                alt="logo"
+              />
             </Box>
           </Link>
           {isMobile ? (
             <Flex alignItems={"center"}>
-              <IconButton aria-label="Search" icon={<SearchIcon />} onClick={handleSearchSubmit} variant="ghost" size="md" rounded={"full"} opacity={0.7} mr={4} />
+              <Input
+                placeholder="Search"
+                size="sm"
+                borderRadius="full"
+                mr={4}
+                value={searchValue}
+                onChange={handleSearchChange}
+                css={{
+                  "::placeholder": {
+                    opacity: 1, // increase placeholder opacity
+                  },
+                }}
+              />
+              <IconButton
+                aria-label="Toggle Dark Mode"
+                icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+                onClick={toggleColorMode}
+                variant="ghost"
+                size="md"
+                rounded={"full"}
+                opacity={0.7}
+                mr={4}
+                color={colorMode === "dark" ? "white" : "black"}
+              />
               <UserProfileMenu />
             </Flex>
           ) : (
@@ -166,27 +246,46 @@ const NotificationsModal = () => {
                 handleSearchSubmit();
               }}
             >
-              <Input placeholder="Search" size="sm" borderRadius="full" mr={4} value={searchValue} onChange={handleSearchChange} />
+              <Input
+                placeholder="Search"
+                size="sm"
+                borderRadius="full"
+                mr={4}
+                value={searchValue}
+                onChange={handleSearchChange}
+                css={{
+                  "::placeholder": {
+                    opacity: 1, // increase placeholder opacity
+                  },
+                }}
+              />
               <Link href="/Create">
-                <IconButton aria-label="Create" icon={<AddIcon />} variant="ghost" size="md" rounded={"full"} opacity={0.7} mr={4} color={colorMode === "dark" ? "white" : "black"} />
+                <IconButton
+                  aria-label="Create"
+                  icon={<AddIcon />}
+                  variant="ghost"
+                  size="md"
+                  rounded={"full"}
+                  opacity={0.7}
+                  mr={3}
+                  color={colorMode === "dark" ? "white" : "black"}
+                />
               </Link>
               <IconButton
-                aria-label="Notifications"
-                icon={<BellIcon />}
+                aria-label="Toggle Dark Mode"
+                icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+                onClick={toggleColorMode}
                 variant="ghost"
                 size="md"
                 rounded={"full"}
                 opacity={0.7}
                 mr={4}
                 color={colorMode === "dark" ? "white" : "black"}
-                onClick={toggleNotifications} // Toggle function
               />
-
               {isUserLoggedIn ? <UserProfileMenu /> : <LoggedOutMenu />}
             </Flex>
           )}
         </Flex>
-        {isNotificationsOpen && <NotificationsModal />}
       </Box>
     </>
   );
--- frontend/pages/profile/MyProfile.tsx
+++ frontend/pages/profile/MyProfile.tsx
@@ -1,11 +1,6 @@
-import * as React from 'react';
-import {useState, useEffect} from 'react';
-import {
-  Container,
-  Box,
-  Stack,
-  useBreakpointValue,
-} from '@chakra-ui/react';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { Container, Box, Stack, useBreakpointValue } from "@chakra-ui/react";
 
 import Navbar from "../../components/shared/Navbar";
 import Header from "../../components/profile/Header";
@@ -17,32 +12,36 @@ import { useSession } from "next-auth/react";
 //const { data: session } = useSession();
 
 const myProfile = () => {
-const isInline = useBreakpointValue({ base: false, md: true, default: false });
+  const isInline = useBreakpointValue({
+    base: false,
+    md: true,
+    default: false,
+  });
 
   const [id, setPodcastId] = useState(1);
 
   const childToParent = (childdata) => {
-    setPodcastId(childdata)
-  }
+    setPodcastId(childdata);
+  };
 
   return (
-      <>
-        <Navbar />
-        <Box display="flex" justifyContent="center" paddingTop="5em">
-          <Stack isInline={isInline} spacing="4" alignItems="flex-start">
-            <Container>
-              <Header/> 
-              {/* Fetch the podcast ID from Podcasts */}
-              <Podcasts childToParent={childToParent}/>
-            </Container>
-            <Container>
-              {/* Pass the podcast ID to MyEpisodes */}
-              <MyEpisodes selectedPodcastId={id}/>
-            </Container>
-          </Stack>
-        </Box>
-      </>
-    );
-  };
+    <>
+      <Navbar />
+      <Box display="flex" justifyContent="center" paddingTop="5em">
+        <Stack isInline={isInline} spacing="4" alignItems="flex-start">
+          <Container>
+            <Header />
+            {/* Fetch the podcast ID from Podcasts */}
+            <Podcasts childToParent={childToParent} />
+          </Container>
+          <Container>
+            {/* Pass the podcast ID to MyEpisodes */}
+            <MyEpisodes selectedPodcastId={id} />
+          </Container>
+        </Stack>
+      </Box>
+    </>
+  );
+};
 
 export default myProfile;
