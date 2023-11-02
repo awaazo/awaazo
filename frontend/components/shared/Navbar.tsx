41e0ae1f39bd043fb9e7d0580f079b9d1a3236c5
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2023-11-02 16:58:59 -0400

    Adjusted dark mode styling and nav bar switching theme text

--- frontend/components/shared/Navbar.tsx
+++ frontend/components/shared/Navbar.tsx
@@ -2,8 +2,32 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { Profile } from "next-auth";
-import { Box, Flex, Avatar, IconButton, Button, Menu, MenuButton, MenuList, MenuItem, MenuDivider, MenuGroup, useColorModeValue, useColorMode, Image, Input, useBreakpointValue, Icon } from "@chakra-ui/react";
-import { MoonIcon, SunIcon, SearchIcon, AddIcon, HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Avatar,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  MenuGroup,
+  useColorModeValue,
+  useColorMode,
+  Image,
+  Input,
+  useBreakpointValue,
+  Icon,
+} from "@chakra-ui/react";
+import {
+  MoonIcon,
+  SunIcon,
+  SearchIcon,
+  AddIcon,
+  HamburgerIcon,
+} from "@chakra-ui/icons";
 import LogoWhite from "../../public/logo_white.svg";
 import LogoBlack from "../../public/logo_black.svg";
 import AuthHelper from "../../helpers/AuthHelper";
@@ -33,23 +57,28 @@ export default function Navbar() {
     // Custom User logged in
     if (!isUserSet) {
       AuthHelper.authMeRequest().then((response) => {
-        if(response.status == 200){
-        setUser(response.userMenuInfo);
-        setIsUserLoggedIn(true); // Set login status to true
-        setIsUserSet(true);
-        setIsLoggedIn(true)
+        if (response.status == 200) {
+          setUser(response.userMenuInfo);
+          setIsUserLoggedIn(true); // Set login status to true
+          setIsUserSet(true);
+          setIsLoggedIn(true);
         }
       });
     }
     // Google User logged in
     else if (session && !isLoggedIn) {
-      AuthHelper.loginGoogleSSO(session.user.email, session.user.name, (session as any).id, session.user.image).then(() => {
+      AuthHelper.loginGoogleSSO(
+        session.user.email,
+        session.user.name,
+        (session as any).id,
+        session.user.image
+      ).then(() => {
         AuthHelper.authMeRequest().then((response) => {
-          if(response.status == 200){
-          setUser(response.userMenuInfo);
-          setIsUserLoggedIn(true); // Set login status to true
-          setIsUserSet(true);
-          setIsLoggedIn(true)
+          if (response.status == 200) {
+            setUser(response.userMenuInfo);
+            setIsUserLoggedIn(true); // Set login status to true
+            setIsUserSet(true);
+            setIsLoggedIn(true);
           }
         });
       });
@@ -71,20 +100,52 @@ export default function Navbar() {
   };
 
   const UserProfileMenu = () => (
-    <Menu>
-      <MenuButton aria-label="loggedInMenu" as={Button} rounded={"full"} variant={"link"} cursor={"pointer"}>
-        {user.avatarUrl === "" ? 
-        <Avatar size={"sm"} src={"https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"} boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)" bg="rgba(255, 255, 255, 0.2)" backdropFilter="blur(10px)" /> : 
-        <Avatar size={"sm"} src={user.avatarUrl} boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)" bg="rgba(255, 255, 255, 0.2)" backdropFilter="blur(10px)" />}
+    <Menu >
+      <MenuButton
+        aria-label="loggedInMenu"
+        as={Button}
+        rounded={"full"}
+        variant={"link"}
+        cursor={"pointer"}
+      >
+        {user.avatarUrl === "" ? (
+          <Avatar
+            size={"sm"}
+            src={
+              "https://images.unsplash.com/photo-1495462911434-be47104d70fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+            }
+            boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)"
+            bg="rgba(255, 255, 255, 0.2)"
+            backdropFilter="blur(10px)"
+          />
+        ) : (
+          <Avatar
+            size={"sm"}
+            src={user.avatarUrl}
+            boxShadow="0px 0px 10px rgba(0, 0, 0, 0.2)"
+            bg="rgba(255, 255, 255, 0.2)"
+            backdropFilter="blur(10px)"
+          />
+        )}
       </MenuButton>
       <MenuList>
-        <MenuItem onClick={toggleColorMode}>{colorMode === "light" ? "Switch to Dark Mode" : "Switch to Light Mode"}</MenuItem>
+        <MenuItem onClick={toggleColorMode}>
+          {colorMode === "light" ? "🌙" : "🔆"}
+        </MenuItem>
         <MenuDivider />
         <MenuGroup>
           <MenuItem>{user.username}</MenuItem>
           <MenuDivider />
-          <MenuItem onClick={() => (window.location.href = "/profile/MyProfile")}>My Account</MenuItem>
-          <MenuItem onClick={() => (window.location.href = "/profile/MyPodcast")}>My Podcast</MenuItem>
+          <MenuItem
+            onClick={() => (window.location.href = "/profile/MyProfile")}
+          >
+            My Account
+          </MenuItem>
+          <MenuItem
+            onClick={() => (window.location.href = "/profile/MyPodcast")}
+          >
+            My Podcast
+          </MenuItem>
           <MenuItem>Payments</MenuItem>
         </MenuGroup>
         <MenuDivider />
@@ -93,7 +154,10 @@ export default function Navbar() {
           <MenuItem>FAQ</MenuItem>
           <MenuDivider />
 
-          <MenuItem onClick={handleLogOut} style={{ color: "red", fontWeight: "bold" }}>
+          <MenuItem
+            onClick={handleLogOut}
+            style={{ color: "red", fontWeight: "bold" }}
+          >
             Logout
           </MenuItem>
         </MenuGroup>
@@ -103,15 +167,26 @@ export default function Navbar() {
 
   const LoggedOutMenu = () => (
     <Menu>
-      <MenuButton menu-id="menuBtn" aria-label="Menu" as={Button} variant={"link"} cursor={"pointer"}>
+      <MenuButton
+        menu-id="menuBtn"
+        aria-label="Menu"
+        as={Button}
+        variant={"link"}
+        cursor={"pointer"}
+      >
         <HamburgerIcon />
       </MenuButton>
       <MenuList>
-        <MenuItem id="loginBtn" onClick={() => (window.location.href = loginPage)}>
+        <MenuItem
+          id="loginBtn"
+          onClick={() => (window.location.href = loginPage)}
+        >
           Login
         </MenuItem>
         <MenuDivider />
-        <MenuItem onClick={() => (window.location.href = registerPage)}>Register</MenuItem>
+        <MenuItem onClick={() => (window.location.href = registerPage)}>
+          Register
+        </MenuItem>
       </MenuList>
     </Menu>
   );
@@ -137,12 +212,24 @@ export default function Navbar() {
         <Flex alignItems={"center"} justifyContent={"space-between"} px={6}>
           <Link href="/">
             <Box maxWidth={"1.5em"} ml={-2}>
-              <Image src={colorMode === "dark" ? LogoWhite.src : LogoBlack.src} alt="logo" />
+              <Image
+                src={colorMode === "dark" ? LogoWhite.src : LogoBlack.src}
+                alt="logo"
+              />
             </Box>
           </Link>
           {isMobile ? (
             <Flex alignItems={"center"}>
-              <IconButton aria-label="Search" icon={<SearchIcon />} onClick={handleSearchSubmit} variant="ghost" size="md" rounded={"full"} opacity={0.7} mr={4} />
+              <IconButton
+                aria-label="Search"
+                icon={<SearchIcon />}
+                onClick={handleSearchSubmit}
+                variant="ghost"
+                size="md"
+                rounded={"full"}
+                opacity={0.7}
+                mr={4}
+              />
               <UserProfileMenu />
             </Flex>
           ) : (
@@ -154,9 +241,25 @@ export default function Navbar() {
                 handleSearchSubmit();
               }}
             >
-              <Input placeholder="Search" size="sm" borderRadius="full" mr={4} value={searchValue} onChange={handleSearchChange} />
+              <Input
+                placeholder="Search"
+                size="sm"
+                borderRadius="full"
+                mr={4}
+                value={searchValue}
+                onChange={handleSearchChange}
+              />
               <Link href="/Create">
-                <IconButton aria-label="Create" icon={<AddIcon />} variant="ghost" size="md" rounded={"full"} opacity={0.7} mr={4} color={colorMode === "dark" ? "white" : "black"} />
+                <IconButton
+                  aria-label="Create"
+                  icon={<AddIcon />}
+                  variant="ghost"
+                  size="md"
+                  rounded={"full"}
+                  opacity={0.7}
+                  mr={4}
+                  color={colorMode === "dark" ? "white" : "black"}
+                />
               </Link>
               {isUserLoggedIn ? <UserProfileMenu /> : <LoggedOutMenu />}
             </Flex>
