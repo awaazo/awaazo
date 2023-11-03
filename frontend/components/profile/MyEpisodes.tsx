174ec91d561dd5eb2c60c110fb92fa696002e5e1
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2023-11-03 14:09:54 -0400

    homepage - Add changes to Frontend branch  (#136)
    * replaced main page with the index.html
    
    * Refactored player bar and adjusted styling
    
    * Adjusted mobile styling
    
    * Update PlayerBar.tsx
    
    * removed currently playing component
    
    * Update ExploreGenres.tsx
    
    Refactored genres code
    
    * Adjusted dark mode styling and nav bar switching theme text
    
    * Update Navbar.tsx
    
    * Update Navbar.tsx
    
    * Update ExploreGenres.tsx
    
    * Update Navbar.tsx
    
    - fixed search bar in mobile view
    - moved dark mode toggle to nav
    
    * Update PlayerBar.tsx
    
    * Added for you page
    
    * Adjusted mobile styling, added recently uplaoded component
    
    * Added for you page
    
    * Update ForYou.tsx
    
    * Adjusted styling for recently upload
    
    * Update ExploreGenres.tsx

--- frontend/components/profile/MyEpisodes.tsx
+++ frontend/components/profile/MyEpisodes.tsx
@@ -1,5 +1,16 @@
 import React from "react";
-import { Box, Tag, Avatar, HStack, Text, Icon, Link, useColorModeValue, Stack, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Tag,
+  Avatar,
+  HStack,
+  Text,
+  Icon,
+  Link,
+  useColorModeValue,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
 
 // Here we have used react-icons package for the icon
 import { FaPlay } from "react-icons/fa";
@@ -12,7 +23,8 @@ const episodes = [
     tags: ["News", "Product", "AI Generated"],
     title: "Episode 1: Build a Modern User Interface with Chakra UI", // Add 'Episode X:' to the title
     content: `In this episode, we discuss how to build a modern user interface with Chakra UI. Lorem Ipsum is simply dummy text of the printing and typesetting industry. simply dummy text...`,
-    userAvatar: "https://d.newsweek.com/en/full/1962972/spacex-owner-tesla-ceo-elon-musk.jpg",
+    userAvatar:
+      "https://d.newsweek.com/en/full/1962972/spacex-owner-tesla-ceo-elon-musk.jpg",
     username: "Elon Musk",
     created_at: "Wed Apr 06 2022",
   },
@@ -21,9 +33,11 @@ const episodes = [
     episodeId: 1,
     type: "podcast", // Add 'type' field for the second episode
     tags: ["Web Development", "Video", "AI Generated"],
-    title: "Episode 1: The Complete Guide to Ruby on Rails Encrypted Credentials", // Add 'Episode X:' to the title
+    title:
+      "Episode 1: The Complete Guide to Ruby on Rails Encrypted Credentials", // Add 'Episode X:' to the title
     content: `In this episode, we dive deep into the complete guide to Ruby on Rails encrypted credentials. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-    userAvatar: "https://www.amacad.org/sites/default/files/person/headshots/oprah.jpg",
+    userAvatar:
+      "https://www.amacad.org/sites/default/files/person/headshots/oprah.jpg",
     username: "Oprah Winfrey",
     created_at: "Sun Apr 03 2022",
   },
@@ -34,14 +48,17 @@ const episodes = [
     tags: ["Web Development", "Audio", "AI Generated"],
     title: "Episode 2: The Future of Web Development", // Add 'Episode X:' to the title
     content: `In this episode, we discuss the future of web development. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-    userAvatar: "https://www.amacad.org/sites/default/files/person/headshots/oprah.jpg",
+    userAvatar:
+      "https://www.amacad.org/sites/default/files/person/headshots/oprah.jpg",
     username: "Oprah Winfrey",
     created_at: "Sun Apr 10 2022",
   },
 ];
 
-export default function MyEpisodes({selectedPodcastId}) {
-  let selectedEpisodes = episodes.filter(episode => episode.podcastId === selectedPodcastId);
+export default function MyEpisodes({ selectedPodcastId }) {
+  let selectedEpisodes = episodes.filter(
+    (episode) => episode.podcastId === selectedPodcastId
+  );
   return (
     <>
       <h1
@@ -60,44 +77,80 @@ export default function MyEpisodes({selectedPodcastId}) {
             direction="column"
             spacing={4}
             p={8}
-            bg={useColorModeValue("gray.100", "gray.800")}
+            style={{
+              backgroundColor: useColorModeValue(
+                "rgba(255, 255, 255, 0.1)",
+                "rgba(0, 0, 0, 0.1)"
+              ),
+              backdropFilter: "blur(10px)",
+            }}
             border="1px solid"
             borderColor="blue.100"
             _hover={{
               borderColor: "blue.300",
-              boxShadow: useColorModeValue("0 4px 6px rgba(160, 174, 192, 0.6)", "0 4px 6px rgba(9, 17, 28, 0.9)"),
+              boxShadow: useColorModeValue(
+                "0 4px 6px rgba(160, 174, 192, 0.6)",
+                "0 4px 6px rgba(9, 17, 28, 0.9)"
+              ),
             }}
             rounded="2em"
           >
             <HStack spacing={2} mb={1}>
               {episode.tags.map((category, index) => (
-                <Tag key={index} colorScheme={useColorModeValue("blackAlpha", "gray")} borderRadius="full">
+                <Tag
+                  key={index}
+                  colorScheme={useColorModeValue("blackAlpha", "gray")}
+                  borderRadius="full"
+                >
                   {category}
                 </Tag>
               ))}
             </HStack>
             <Box textAlign="left">
-              <Link fontSize="xl" lineHeight={1.2} fontWeight="bold" w="100%" _hover={{ color: "blue.400", textDecoration: "underline" }}>
+              <Link
+                fontSize="xl"
+                lineHeight={1.2}
+                fontWeight="bold"
+                w="100%"
+                _hover={{ color: "blue.400", textDecoration: "underline" }}
+              >
                 {episode.title}
               </Link>
-              <Text fontSize="md" color="gray.500" noOfLines={2} lineHeight="normal">
+              <Text fontSize="md" lineHeight="normal">
                 {episode.content}
               </Text>
             </Box>
             <Box>
-              <Avatar size="sm" title="Author" mb={2} src={episode.userAvatar} />
+              <Avatar
+                size="sm"
+                title="Author"
+                mb={2}
+                src={episode.userAvatar}
+              />
 
-              <Stack justify="space-between" direction={{ base: "column", sm: "row" }}>
+              <Stack
+                justify="space-between"
+                direction={{ base: "column", sm: "row" }}
+              >
                 {/* author credentials section */}
                 <Box>
                   <Text fontSize="sm" fontWeight="bold">
                     {episode.username}
                   </Text>
-                  <Text fontSize="sm" color="gray.500">
-                    {episode.created_at}
-                  </Text>
+                  <Text fontSize="sm">{episode.created_at}</Text>
                 </Box>
-                <HStack as={Link} spacing={1} p={1} alignItems="center" height="2rem" w="max-content" margin="auto 0" rounded="lg" color="blue.400" _hover={{ bg: useColorModeValue("gray.200", "gray.700") }}>
+                <HStack
+                  as={Link}
+                  spacing={1}
+                  p={1}
+                  alignItems="center"
+                  height="2rem"
+                  w="max-content"
+                  margin="auto 0"
+                  rounded="lg"
+                  color="blue.400"
+                  _hover={{ bg: useColorModeValue("gray.200", "gray.700") }}
+                >
                   <Icon as={FaPlay} w={6} h={6} />
                 </HStack>
               </Stack>
