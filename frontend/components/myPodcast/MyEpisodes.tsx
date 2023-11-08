5347e0edba568177a8233c2c73851a0c7c55223f
Radwanbaba13 <radwangbaba@gmail.com>
2023-11-07 23:05:30 -0500

    Delete Podcast Manager

--- frontend/components/myPodcast/MyEpisodes.tsx
+++ frontend/components/myPodcast/MyEpisodes.tsx
@@ -1,22 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Box,
   Flex,
-  Image,
-  Text,
-  Tag,
+  Avatar,
   IconButton,
-  useColorMode,
-  useBreakpointValue,
-  Icon,
+  Tag,
+  MenuGroup,
   Tooltip,
   useColorModeValue,
+  useColorMode,
+  useDisclosure,
+  useBreakpointValue,
+  Text,
+  Icon,
+  Button,
+  Collapse,
+  SimpleGrid,
+  VStack,
+  Image,
+  HStack,
+  Wrap,
+  WrapItem,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spacer,
 } from "@chakra-ui/react";
-import { MdSettings } from "react-icons/md";
+import { MdEdit, MdDelete } from "react-icons/md";
+
+import EditEpisodeForm from "../myPodcast/EditEpisodeForm";
+import PodcastHelper from "../../helpers/PodcastHelper";
 
 const Episode = ({ episode }) => {
-  const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode } = useColorMode();
   const isMobile = useBreakpointValue({ base: true, md: false });
+
+  const formatDuration = (duration) => {
+    const minutes = Math.floor(duration / 60);
+    const seconds = Math.floor(duration % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
+  // Edit Episode Modal
+  //----------------------------------------------------------------------
+  const [isEditEpisodeModalOpen, setIsEditEpisodeModalOpen] = useState(false);
+  const [currentEditingEpisode, setCurrentEditingEpisode] = useState(null);
+
+  // State for managing modal visibility and the current episode
+  const [isModalEpisodeOpen, setIsModalEpisodeOpen] = useState(false);
+  const [currentEpisode, setCurrentEpisode] = useState(null);
+
+  const openEditEpisodeModal = (episode) => {
+    setCurrentEpisode(episode);
+    setIsModalEpisodeOpen(true);
+  };
+  const closeEditEpisodeModal = () => {
+    setIsModalEpisodeOpen(false);
+    setCurrentEpisode(null);
+  };
+  //----------------------------------------------------------------------
+
+  // For delete pop up
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [isDeleting, setDeleting] = useState(false);
+
+  // Form errors
+  const [episodeError, setEpisodeError] = useState("");
+  // Handle Deletion of podcast
+  const handleDelete = async (episodeId) => {
+    setDeleting(true);
+    // Create request object
+    const response = await PodcastHelper.deleteEpisode(episode.id);
+    console.log(response);
+    if (response.status == 200) {
+      window.location.reload();
+    } else {
+      setEpisodeError("Episode cannot be deleted");
+    }
+    onClose();
+    setDeleting(false);
+  };
+
   return (
     <Flex
       p={4}
@@ -32,7 +100,7 @@ const Episode = ({ episode }) => {
       <Box position="relative" mr={5}>
         <Image
           boxSize={isMobile ? "90px" : "125px"}
-          src={episode.thumbnail}
+          src={episode.thumbnailUrl}
           borderRadius="10%"
           mt={1}
         />
@@ -40,7 +108,7 @@ const Episode = ({ episode }) => {
       <Flex direction="column" flex={1}>
         {/* Episode Name */}
         <Text fontWeight="medium" fontSize={isMobile ? "sm" : "2xl"}>
-          {episode.name}{" "}
+          {episode.episodeName}
           {episode.isExplicit && (
             <Tag
               size="sm"
@@ -50,6 +118,8 @@ const Episode = ({ episode }) => {
               Explicit
             </Tag>
           )}
+          <Text fontSize={isMobile ? "md" : "md"}>🎧 {episode.playCount}</Text>
+          <Text fontSize={isMobile ? "md" : "md"}>❤️ {episode.playCount}</Text>
         </Text>
         {/* Episode Details */}
         <Flex
@@ -59,20 +129,15 @@ const Episode = ({ episode }) => {
         >
           <Text>{episode.description}</Text>
           <Text fontWeight="bold" fontSize={isMobile ? "12px" : "md"}>
-            Duration: {episode.duration}
-          </Text>
-          <Text fontWeight="bold" fontSize={isMobile ? "12px" : "md"}>
-            Release Date: {episode.releaseDate}
-          </Text>
-          <Text fontWeight="bold" fontSize={isMobile ? "12px" : "md"}>
-            Play Count: {episode.playCount}
+            Duration: {formatDuration(episode.duration)}
           </Text>
         </Flex>
       </Flex>
-      {/* Manage Section */}
-      <Flex alignItems="center">
+
+      {/* Edit and Delete Buttons */}
+      <Flex alignItems="flex-start">
         <Box>
-          <Tooltip label="Manage" aria-label="Manage Tooltip">
+          <Tooltip label="Edit" aria-label="Edit Tooltip">
             <IconButton
               variant="ghost"
               fontSize={isMobile ? "2xl" : "3xl"}
@@ -80,13 +145,81 @@ const Episode = ({ episode }) => {
               rounded={"full"}
               opacity={0.7}
               color={colorMode === "dark" ? "white" : "black"}
-              aria-label="Manage Episode"
-              icon={<Icon as={MdSettings} />}
-              onClick={() => console.log(episode.id, episode.name)}
+              aria-label="Edit Episode"
+              icon={<Icon as={MdEdit} />}
+              onClick={() => openEditEpisodeModal(episode)}
+            />
+          </Tooltip>
+        </Box>
+        <Box>
+          <Tooltip label="Delete" aria-label="Delete Tooltip">
+            <IconButton
+              variant="ghost"
+              fontSize={isMobile ? "2xl" : "3xl"}
+              rounded={"full"}
+              opacity={0.7}
+              color={colorMode === "dark" ? "white" : "black"}
+              aria-label="Delete Episode"
+              icon={<Icon as={MdDelete} />}
+              onClick={onOpen}
             />
           </Tooltip>
         </Box>
       </Flex>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Confirm Deletion</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            Are you sure you want to delete the episode "{episode.episodeName}".{" "}
+            <br />
+            This action cannot be undone
+          </ModalBody>
+          <ModalFooter>
+            <Button variant="ghost" onClick={onClose}>
+              Cancel
+            </Button>
+            <Button
+              colorScheme="red"
+              ml={3}
+              onClick={() => handleDelete(episode.episodeId)}
+            >
+              Delete
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={isModalEpisodeOpen} onClose={closeEditEpisodeModal}>
+        <ModalOverlay backdropFilter="blur(10px)" />
+        <ModalContent
+          boxShadow="dark-lg"
+          backdropFilter="blur(40px)"
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          marginTop={"10%"}
+          padding={"2em"}
+        >
+          <ModalCloseButton />
+          <ModalBody>
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <VStack
+                spacing={5}
+                align="center"
+                backgroundColor={"transparent"}
+              >
+                <Text>Edit Episode: {currentEpisode?.title}</Text>
+
+                <EditEpisodeForm />
+              </VStack>
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 };
