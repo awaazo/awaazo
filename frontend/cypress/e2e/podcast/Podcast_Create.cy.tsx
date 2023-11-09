0cd3f5ed12371c6606c69f35b7aa836dfab79b7e
sFranchetto <31280516+sFranchetto@users.noreply.github.com>
2023-11-08 21:47:11 -0500

    User auth passing, have to finish podcast and episode create

--- frontend/cypress/e2e/podcast/Podcast_Create.cy.tsx
+++ frontend/cypress/e2e/podcast/Podcast_Create.cy.tsx
@@ -6,8 +6,7 @@ describe ('Postcast_Create', () => {
     it('Should successfully create a Podcast', function () {
         cy.visit('/');
         cy.url().should('include', '/');
-        cy.Quick_register();
-        //cy.login();
+        cy.login();
         cy.wait(500);
         cy.get('button[aria-label="Create"]').click();
         cy.url().should('include', '/Create');
@@ -16,13 +15,13 @@ describe ('Postcast_Create', () => {
         cy.wait(500);
         cy.get('input[type="file"]').attachFile(filepath_Podcast_cover);
         cy.wait(550);
-        cy.get('input[id="podcastName"]').type('F1 Legendsss');
+        cy.get('input[id="podcastName"]').type('F1 Legends');
         cy.get('textarea[id="description"]').type('A podcast about F1 veterans and their rise to glory.');
         cy.get(':nth-child(5) > .chakra-button').click();
         cy.get(':nth-child(7) > .chakra-button').click();
         cy.get(':nth-child(10) > .chakra-button').click();
         cy.get('button[id=createBtn]').click(); 
         cy.url().should('include', '/Create');
-        cy.contains('F1 Legendsss');
+        cy.contains('F1 Legends');
     });
 });
