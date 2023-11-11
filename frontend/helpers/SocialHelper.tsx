48b68a548d079449f64fa722ba93044c617621f6
Radwanbaba13 <radwangbaba@gmail.com>
2023-11-11 14:12:28 -0500

    Updated Helpers to Like/Unlike episode

--- frontend/helpers/SocialHelper.tsx
+++ frontend/helpers/SocialHelper.tsx
@@ -1,201 +1,179 @@
 import axios from "axios";
 import EndpointHelper from "./EndpointHelper";
+import { EpisodeCommentRequest } from "../utilities/Requests";
 import {
-    EpisodeCommentRequest,
-    EpisodeLikeRequest
-} from "../utilities/Requests";
-import {
-    BaseResponse,
-    GetMyPodcastResponse,
-    MyPodcastResponse,
-    GetMyEpisodeResponse
+  BaseResponse,
+  GetMyPodcastResponse,
+  MyPodcastResponse,
+  GetMyEpisodeResponse,
 } from "../utilities/Responses";
 import { request } from "http";
 
-export default class SocialHelper{
-    static getEpisodeComments() {
-        throw new Error("Method not implemented.");
+export default class SocialHelper {
+  static getEpisodeComments() {
+    throw new Error("Method not implemented.");
+  }
+
+  /**
+   * creates a new episode comment
+   * @param requestData
+   * @param episodeId
+   * @param replyToCommentId
+   * @param commentId
+   * @returns
+   */
+
+  // Post a new comment
+  public static postEpisodeComment = async (
+    requestData: EpisodeCommentRequest,
+    episodeOrCommentId,
+  ): Promise<BaseResponse> => {
+    const options = {
+      method: "POST",
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+      },
+      data: requestData,
+      url: EndpointHelper.getEpisodeCommentEndpoint(episodeOrCommentId),
+      withCredentials: true, // This will send the session cookie with the request
+      cache: false,
+    };
+
+    try {
+      console.debug("Sending the following postEpisodeComment...");
+      console.debug(options);
+
+      console.log(options);
+      // Send the request and wait for the response.
+      const requestResponse = await axios(options);
+
+      console.debug("Received the following postEpisodeComment...");
+      console.debug(requestResponse);
+
+      // Return the response.
+      return {
+        status: requestResponse.status,
+        message: requestResponse.statusText,
+      };
+    } catch (error) {
+      return {
+        status: error.response?.status,
+        message: error.response?.statusText,
+      };
     }
+  };
 
-    /**
-     * creates a new episode comment
-     * @param requestData
-     * @param episodeId
-     * @param replyToCommentId
-     * @param commentId
-     * @returns
-     */
-
-    // Post a new comment
-    public static postEpisodeComment = async (
-        requestData: EpisodeCommentRequest,
-        episodeOrCommentId,
-    ): Promise<BaseResponse> => {
-        const options = {
-            method: "POST",
-            headers: {
-                accept: "*/*",
-                "Content-Type": "application/json"
-
-            },
-            data: requestData,
-            url: EndpointHelper.getEpisodeCommentEndpoint(episodeOrCommentId),
-            withCredentials: true, // This will send the session cookie with the request
-            cache: false
-        };
-
-        try{
-            console.debug("Sending the following postEpisodeComment...");
-            console.debug(options);
-
-            console.log(options);
-            // Send the request and wait for the response.
-            const requestResponse = await axios(options);
-
-            console.debug("Received the following postEpisodeComment...");
-            console.debug(requestResponse);
-
-            // Return the response.
-            return {
-                status: requestResponse.status,
-                message: requestResponse.statusText,
-            };
-        } catch (error) {
-            return {
-                status: error.response?.status,
-                message: error.response?.statusText,
-            };
-        }
+  public static deleteComment = async (commentId): Promise<BaseResponse> => {
+    // Create the request options.
+    const options = {
+      method: "DELETE",
+      url: EndpointHelper.getEpisodeCommentDeleteEndpoint(commentId),
+      headers: {
+        accept: "*/*",
+      },
+      withCredentials: true, // This will send the session cookie with the request
+      cache: false,
     };
 
-    public static deleteComment = async (commentId): Promise<BaseResponse> => {
-        // Create the request options.
-        const options = {
-            method: "DELETE",
-            url: EndpointHelper.getEpisodeCommentDeleteEndpoint(commentId),
-            headers: {
-                accept: "*/*",
-            },
-            withCredentials: true, // This will send the session cookie with the request
-            cache: false
-        };
+    try {
+      console.debug("Sending the following deleteComment...");
+      console.debug(options);
 
-        try {
-            console.debug("Sending the following deleteComment...");
-            console.debug(options);
+      console.log(options);
+      // Send the request and wait for the response.
+      const requestResponse = await axios(options);
 
-            console.log(options);
-            // Send the request and wait for the response.
-            const requestResponse = await axios(options);
+      console.debug("Received the following deleteComment...");
+      console.debug(requestResponse);
 
-            console.debug("Received the following deleteComment...");
-            console.debug(requestResponse);
+      // Return the response.
+      return {
+        status: requestResponse.status,
+        message: requestResponse.statusText,
+      };
+    } catch (error) {
+      return {
+        status: error.response?.status,
+        message: error.response?.statusText,
+      };
+    }
+  };
 
-            // Return the response.
-            return {
-                status: requestResponse.status,
-                message: requestResponse.statusText,
-            };
-        } catch (error) {
-            return {
-                status: error.response?.status,
-                message: error.response?.statusText,
-            };
-        }
+  // Post a new like
+  public static postEpisodeLike = async (
+    episodeOrCommentId,
+  ): Promise<BaseResponse> => {
+    const options = {
+      method: "POST",
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+      },
+      url: EndpointHelper.getEpisodeLikeEndpoint(episodeOrCommentId),
+      withCredentials: true,
+      cache: false,
     };
 
-    // Post a new like
-    public static postEpisodeLike = async (
-        requestData: EpisodeLikeRequest,
-        episodeOrCommentId,
-    ): Promise<BaseResponse> => {
-        const options = {
-            method: "POST",
-            headers: {
-                accept: "*/*",
-                "Content-Type": "application/json"
-            },
-            data: requestData,
-            url: EndpointHelper.getEpisodeLikeEndpoint(episodeOrCommentId),
-            withCredentials: true, 
-            cache: false
-        };
+    try {
+      console.debug("Sending the following postEpisodeLike...");
+      console.debug(options);
 
-        try {
-            console.debug("Sending the following postEpisodeLike...");
-            console.debug(options);
+      console.log(options);
+      // Send the request and wait for the response.
+      const requestResponse = await axios(options);
 
-            console.log(options);
-            // Send the request and wait for the response.
-            const requestResponse = await axios(options);
+      console.debug("Received the following postEpisodeLike...");
+      console.debug(requestResponse);
 
-            console.debug("Received the following postEpisodeLike...");
-            console.debug(requestResponse);
+      // Return the response.
+      return {
+        status: requestResponse.status,
+        message: requestResponse.statusText,
+      };
+    } catch (error) {
+      return {
+        status: error.response?.status,
+        message: error.response?.statusText,
+      };
+    }
+  };
 
-            // Return the response.
-            return {
-                status: requestResponse.status,
-                message: requestResponse.statusText,
-            };
-        } catch (error) {
-            return {
-                status: error.response?.status,
-                message: error.response?.statusText,
-            };
-        }
+  // Delete a like
+  public static deleteEpisodeLike = async (
+    episodeOrCommentId,
+  ): Promise<BaseResponse> => {
+    const options = {
+      method: "DELETE",
+      url: EndpointHelper.getEpisodeUnlikeEndpoint(episodeOrCommentId),
+      headers: {
+        accept: "*/*",
+      },
+      withCredentials: true,
+      cache: false,
     };
 
-    // Delete a like
-    public static deleteEpisodeLike = async (
-        episodeOrCommentId,
-    ): Promise<BaseResponse> => {
-        const options = {
-            method: "DELETE",
-            url: EndpointHelper.getEpisodeUnlikeEndpoint(episodeOrCommentId),
-            headers: {
-                accept: "*/*",
-            },
-            withCredentials: true, 
-            cache: false
-        };
+    try {
+      console.debug("Sending the following deleteEpisodeLike...");
+      console.debug(options);
 
-        try {
-            console.debug("Sending the following deleteEpisodeLike...");
-            console.debug(options);
+      console.log(options);
+      // Send the request and wait for the response.
+      const requestResponse = await axios(options);
 
-            console.log(options);
-            // Send the request and wait for the response.
-            const requestResponse = await axios(options);
+      console.debug("Received the following deleteEpisodeLike...");
+      console.debug(requestResponse);
 
-            console.debug("Received the following deleteEpisodeLike...");
-            console.debug(requestResponse);
-
-            // Return the response.
-            return {
-                status: requestResponse.status,
-                message: requestResponse.statusText,
-            };
-        } catch (error) {
-            return {
-                status: error.response?.status,
-                message: error.response?.statusText,
-            };
-        }
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      // Return the response.
+      return {
+        status: requestResponse.status,
+        message: requestResponse.statusText,
+      };
+    } catch (error) {
+      return {
+        status: error.response?.status,
+        message: error.response?.statusText,
+      };
+    }
+  };
 }
