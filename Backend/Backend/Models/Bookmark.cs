69806d4f8d45d5bc7f6813fdbf28941b44a92e20
Mathieu Trudeau <46968018+mathieutrudeau@users.noreply.github.com>
2023-11-18 17:20:38 -0500

    Revert "Added CRUD API routes for user bookmarks"

--- Backend/Backend/Models/Bookmark.cs
+++ Backend/Backend/Models/Bookmark.cs
@@ -1,27 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Backend.Infrastructure;
 
 namespace Backend.Models;
 
 public class Bookmark : BaseEntity
 {
-    private readonly AppDbContext _db;
 
-    public Bookmark(AppDbContext db) 
+    public Bookmark() 
     {
         Title = string.Empty;
         Note = string.Empty;
-        _db = db;
     }
 
     [Key]
     public Guid Id { get; set; }
 
-    public User? User => _db.Users.FirstOrDefault(u => u.Id == UserId);
+    public User User { get; set; } = null!;
     
     public Guid UserId { get; set; }
 
-    public Episode? Episode => _db.Episodes.FirstOrDefault(e => e.Id == EpisodeId);
+    public Episode Episode { get; set; } = null!;
     
     public Guid EpisodeId { get; set; }
     
