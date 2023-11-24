975f86e739d80c6ec1cbc743ae1995de45e397e9
Ahed Memas <ahed.mimas@gmail.com>
2023-11-23 20:06:50 -0500

    updated Mysubscriptions UI

--- frontend/components/explore/MySubscriptions.tsx
+++ frontend/components/explore/MySubscriptions.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, FC } from "react";
 import SubscribeHelper from "../../helpers/SubscribeHelper";
 import { MySubscriptions } from "../../utilities/Interfaces";
 import Link from "next/link";
-import { Stack, Flex, Avatar, Text, Box, useColorModeValue } from "@chakra-ui/react";
+import { Stack, Flex, Avatar, Text, Box, useColorModeValue, Badge, useToken  } from "@chakra-ui/react";
 
 const Subscriptions: FC = () => {
   const [subscriptions, setSubscriptions] = useState<MySubscriptions[]>([]);
@@ -20,40 +20,35 @@ const Subscriptions: FC = () => {
     fetchSubscriptions();
   }, []);
 
+  const [blue500, gray300, gray500, gray700] = useToken("colors", ["blue.500", "gray.300", "gray.500", "gray.700"]);
+
   return (
-    <Stack spacing={4} alignItems="center" 
-        m={14} 
-        ml={82}
-        borderRadius="md"
-        border="1px"
-        borderColor="gray.200"
-        p={3}
-        >
-      <Text fontWeight="bold">My Subscriptions</Text>
+    <Stack spacing={4} alignItems="center" m={14} borderRadius="md" border="1px" borderColor="gray.200" p={3} w="650px">
+      <Text fontWeight="bold" fontSize="xl" mb={4}>My Subscriptions</Text>
       {subscriptions.map((subscription) => (
         <Box
           key={subscription.id}
-          p={2}
-          borderRadius="md"
+          p={4}
+          borderRadius="full"
           border="1px"
           borderColor="gray.200"
-          w="300px" // Fixed width
-          _hover={{ borderColor: "gray.300", boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.1)" }}
-          bg={useColorModeValue("white", "gray.700")}
+          w="100%"
+          _hover={{ borderColor: blue500, boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)" }}
+          bg={useColorModeValue("white", gray700)}
         >
-          <Link href={`/Explore/${subscription.id}`}>
-            <Flex
-              as="a"
-              alignItems="center"
-              borderRadius="full"
-            >
-              <Avatar
-                size="md"
-                src={subscription.coverArtUrl}
-              />
-              <Flex direction="column" ml={3}>
-                <Text fontWeight="bold">{subscription.name}</Text>
-                <Text fontSize="sm">{subscription.description}</Text>
+          <Link href={`/Explore/${subscription.id}`} passHref>
+            <Flex as="a" alignItems="center" justifyContent="space-between" textDecoration="none">
+              <Flex alignItems="center">
+                <Avatar size="lg" src={subscription.coverArtUrl} mr={4} />
+                <Flex direction="column" justifyContent="center">
+                  <Text fontWeight="bold" color={blue500}>{subscription.name}</Text>
+                  <Text fontSize="sm" color={gray300} mt={1}>{subscription.description}</Text>
+                  <Badge colorScheme={subscription.isExplicit ? "red" : "green"} mt={2}>{subscription.isExplicit ? "Explicit" : "Implicit"}</Badge>
+                </Flex>
+              </Flex>
+              <Flex direction="column" alignItems="flex-end">
+                <Text fontSize="sm" color={gray500}>Average Rating: {subscription.averageRating}</Text>
+                <Text fontSize="sm" color={gray500}>Total Ratings: {subscription.totalRatings}</Text>
               </Flex>
             </Flex>
           </Link>
