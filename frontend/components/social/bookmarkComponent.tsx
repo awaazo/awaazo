e3fadb01335a8ff327f7f83d282a17715343c6bf
soni2261 <soniarahal12@gmail.com>
2023-11-25 16:42:53 -0500

    fetching bookmark details

--- frontend/components/social/bookmarkComponent.tsx
+++ frontend/components/social/bookmarkComponent.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { Button, Icon, Tooltip } from "@chakra-ui/react";
 import { CiBookmarkPlus } from "react-icons/ci";
 import SocialHelper from "../../helpers/SocialHelper";
+import AuthHelper from "../../helpers/AuthHelper";
+import PodcastHelper from "../../helpers/PodcastHelper";
+import { Bookmark } from "../../utilities/Interfaces";
 
 // This component represents a like button for an episode or comment
 const BookmarkComponent = ({ episodeId, selectedTimestamp}) => {
@@ -9,6 +12,7 @@ const BookmarkComponent = ({ episodeId, selectedTimestamp}) => {
 
 
   const [isBookmarked, setIsBookmarked] = useState(false);
+  const [bookmarks, setBookmarks] = useState<Bookmark[]>([]);
 
 //   useEffect(() => {
 //     const fetchData = async () => {
@@ -22,6 +26,58 @@ const BookmarkComponent = ({ episodeId, selectedTimestamp}) => {
 
 //     fetchData();
 //   }, [episodeId]);
+
+
+  // Fetch episode details and transform comments when the modal is opened
+  useEffect(() => {
+
+      const fetchEpisodeDetails = async () => {
+        // AuthHelper.authMeRequest().then((response) => {
+        //   if (response.status == 200) {
+        //     setUser(response.userMenuInfo);
+        //   }
+        // });
+        const response = await PodcastHelper.getEpisodeById(
+          episodeId,
+        );
+        if (response.status === 200) {
+          if (response.episode) {
+            // Transform the bookmarks to match our format
+            const transformedBookmarks = response.episode.bookmarks.map(
+              (bookmark) => ({
+                id: bookmark.id,
+                title: bookmark.title,
+                note: bookmark.note,
+                timestamp: bookmark.timestamp,
+              }),
+            );
+            setBookmarks(transformedBookmarks);
+          }
+        } else {
+          console.error("Error fetching episode details:", response.message);
+        }
+      };
+      fetchEpisodeDetails();
+
+  }, [episodeId]);
+
+
+
+// Add a new bookmark
+// const handleAddBookmark = async () => {
+//   if (newComment.trim()) {
+//     const response = await SocialHelper.postEpisodeComment(
+//       newComment,
+//       episodeIdOrCommentId,
+//     );
+//     if (response.status === 200) {
+//       // Update the UI to reflect the new bookmark
+//       setBookmark(selectedTimestamp);
+//     } else {
+//       console.log("Error posting bookmark:", response.message);
+//     }
+//   }
+// };
 
   // Function to handle the bookmark/remove bookmark action
   const handleBookmark = () => {
