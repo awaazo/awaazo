3064bd9c57bfbfdb51868c92040f2b73d0f8b6d0
Radwanbaba13 <radwangbaba@gmail.com>
2023-12-19 00:25:28 -0500

    Added shared component for displaying playlists

---
+++ frontend/components/Cards/PlaylistCard.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { convertTime } from "../../utilities/commonUtils";
+import { RiPlayList2Fill } from "react-icons/ri";
+import NexLink from "next/link";
+
+const PlaylistCard = ({ playlist }) => {
+  return (
+    <Flex
+      className="hoverEffect"
+      p={4}
+      mt={3}
+      width="100%"
+      borderRadius="15px"
+      overflow="hidden"
+      boxShadow="sm"
+      cursor="pointer"
+      transition="transform 0.3s"
+      _hover={{
+        transform: "scale(1.05)",
+      }}
+    >
+      <Flex direction="column" flex={1}>
+        <NexLink href={`/Playlist/${playlist.id}`}>
+          <Flex justifyContent="space-between" mb={2} align="center">
+            {/* Privacy */}
+            <Flex alignItems="center">
+              <RiPlayList2Fill size={"25px"} />
+              <Text fontWeight="medium" fontSize="xl" ml={2}>
+                {playlist.name}
+              </Text>
+            </Flex>
+            {/* Playlist Name */}
+            <Text fontWeight="medium" fontSize="sm">
+              {playlist.privacy}
+            </Text>
+          </Flex>
+          {/* Playlist Details */}
+          <Box fontSize="sm" color="gray.500">
+            <Text>{playlist.description}</Text>
+            <Flex>
+              <Box flex={1}>
+                <Text>
+                  <strong>Number of Episodes:</strong>{" "}
+                  {playlist.numberOfEpisodes}
+                </Text>
+                <Text>
+                  <strong>Duration:</strong> {convertTime(playlist.duration)}
+                </Text>
+              </Box>
+              <Box>
+                <Text>
+                  <strong>Created At:</strong>{" "}
+                  {new Date(playlist.createdAt).toLocaleDateString()}
+                </Text>
+                <Text>
+                  <strong>Updated At:</strong>{" "}
+                  {new Date(playlist.updatedAt).toLocaleDateString()}
+                </Text>
+              </Box>
+            </Flex>
+          </Box>
+        </NexLink>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default PlaylistCard;
