6dfdf70e7c71cb096ef069d1c261affc1dd416d0
sFranchetto <31280516+sFranchetto@users.noreply.github.com>
2023-12-20 14:15:08 -0500

    quick fix

--- frontend/cypress/e2e/social/Like.cy.tsx
+++ frontend/cypress/e2e/social/Like.cy.tsx
@@ -26,6 +26,7 @@ describe ('Like', () => {
         cy.get('[data-cy="podcast-card-F2 legends"]').should('be.visible').click( {timeout:5000} )
         cy.wait(250);
         cy.get('[data-cy="likes-on-This is a very long episo-1"]').should('be.visible').click( {timeout:5000} )
+        cy.wait(250);
         cy.visit('/').url().should('include', '/');
         cy.wait(400);
         cy.get('[data-cy="search-input-web"]').should('be.visible').type('F2{enter}');
