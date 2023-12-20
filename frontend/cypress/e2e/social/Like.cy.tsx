d67c176d8a5b96b700a5cb87ae146f4545e19a85
sFranchetto <31280516+sFranchetto@users.noreply.github.com>
2023-12-20 14:38:18 -0500

    quick fix

--- frontend/cypress/e2e/social/Like.cy.tsx
+++ frontend/cypress/e2e/social/Like.cy.tsx
@@ -10,28 +10,29 @@ describe ('Like', () => {
     it('Should like a Podcast', () => {
         cy.get('[data-cy="search-input-web"]').should('be.visible').type('F2{enter}');
         cy.get('[data-cy="podcast-card-F2 legends"]').should('be.visible').click( {timeout:5000} )
-        cy.wait(250);
+        cy.wait(500);
         cy.get('[data-cy="likes-on-This is a very long episo-0"]').should('be.visible').click( {timeout:5000} )
         cy.visit('/').url().should('include', '/');
         cy.wait(400);
         cy.get('[data-cy="search-input-web"]').should('be.visible').type('F2{enter}');
         cy.get('[data-cy="podcast-card-F2 legends"]').should('be.visible').click( {timeout:5000} )
-        cy.wait(250);
+        cy.wait(500);
         cy.get('[data-cy="likes-on-This is a very long episo-1"]').should('be.visible');
+        
     });
 
     it('Should unlike a Podcast', () => {
         cy.wait(400);
         cy.get('[data-cy="search-input-web"]').should('be.visible').type('F2{enter}');
         cy.get('[data-cy="podcast-card-F2 legends"]').should('be.visible').click( {timeout:5000} )
-        cy.wait(250);
+        cy.wait(500);
         cy.get('[data-cy="likes-on-This is a very long episo-1"]').should('be.visible').click( {timeout:5000} )
         cy.wait(250);
         cy.visit('/').url().should('include', '/');
         cy.wait(400);
         cy.get('[data-cy="search-input-web"]').should('be.visible').type('F2{enter}');
         cy.get('[data-cy="podcast-card-F2 legends"]').should('be.visible').click( {timeout:5000} )
-        cy.wait(250);
+        cy.wait(500);
         cy.get('[data-cy="likes-on-This is a very long episo-0"]').should('be.visible');
     });
 });
