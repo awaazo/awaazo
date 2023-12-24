1d675f918602297f49f40b4ddec92ad610c8e5ba
Shadi <51339220+shadijiha@users.noreply.github.com>
2023-12-24 00:07:28 -0500

    Update PodcastController.cs

--- Backend/Backend/Controllers/PodcastController.cs
+++ Backend/Backend/Controllers/PodcastController.cs
@@ -278,6 +278,25 @@ public class PodcastController : ControllerBase
         }
     }
 
+    [HttpGet("{podcastId}/metrics")]
+    public async Task<ActionResult> GetMetrics(Guid podcastId) {
+        this.LogDebugControllerAPICall(_logger);
+
+        try {
+            // Identify User from JWT Token
+            User? user = await _authService.IdentifyUserAsync(HttpContext);
+            // If User is not found, return 404
+            if (user is null)
+                return NotFound("User does not exist.");
+
+            return Ok(await _podcastService.GetMetrics(user, podcastId));
+        }
+        catch (Exception e) {
+            this.LogErrorAPICall(_logger, e);
+            return BadRequest(e.Message);
+        }
+    }
+    
     #endregion 
 
     #region Episode
