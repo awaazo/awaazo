105feb24e66fa7792dda67f9d29f54fc78d38f23
Shadi <51339220+shadijiha@users.noreply.github.com>
2023-12-30 03:24:24 -0500

    Create ForgetPasswordToken.cs

---
+++ Backend/Backend/Models/ForgetPasswordToken.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend.Models;
+
+public class ForgetPasswordToken : BaseEntity
+{
+    public Guid UserId { get; set; } = Guid.Empty;
+    public string Token { get; set; } = string.Empty;
+    
+    public ForgetPasswordToken() {}
+
+    public ForgetPasswordToken(User user) {
+        UserId = user.Id;
+        Token = Guid.NewGuid().ToString() +
+                Guid.NewGuid() + 
+                Guid.NewGuid();
+    }
+}
