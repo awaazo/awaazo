3403a92803d9d6455738f2280b2c33ddc4a00d73
Shadi <51339220+shadijiha@users.noreply.github.com>
2023-12-30 03:24:21 -0500

    Create EmailService.cs

---
+++ Backend/Backend/Services/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Backend.Services;
+
+public class EmailService : IDisposable
+{
+    private readonly SmtpClient _client;
+
+    public EmailService(IConfiguration config) {
+        _client = new SmtpClient() {
+            Port = int.Parse(config["Smtp:Port"]!),
+            Credentials = new NetworkCredential(config["Smtp:Username"], config["Smtp:Password"]),
+            EnableSsl = true
+        };
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <exception cref="">Throws exception if Smtp server is not reachable</exception>
+    public void Send(MailMessage message) {
+        _client.Send(message);
+        message.Dispose();
+    }
+    
+    public void Dispose() {
+        _client.Dispose();
+    }
+}
