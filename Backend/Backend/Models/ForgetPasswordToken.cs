0d7e024c2999ec6c881ecf7b6954609e914122b3
Shadi <shadosite@gmail.com>
2023-12-31 23:52:28 -0500

    Update ForgetPasswordToken.cs

--- Backend/Backend/Models/ForgetPasswordToken.cs
+++ Backend/Backend/Models/ForgetPasswordToken.cs
@@ -1,13 +1,22 @@
-﻿namespace Backend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models;
 
 public class ForgetPasswordToken : BaseEntity
 {
+    [Key]
+    public Guid Id { get; set; }
     public Guid UserId { get; set; } = Guid.Empty;
     public string Token { get; set; } = string.Empty;
-    
-    public ForgetPasswordToken() {}
 
-    public ForgetPasswordToken(User user) {
+    public ForgetPasswordToken()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public ForgetPasswordToken(User user)
+    {
+        Id = Guid.NewGuid();
         UserId = user.Id;
         Token = Guid.NewGuid().ToString() +
                 Guid.NewGuid() + 
