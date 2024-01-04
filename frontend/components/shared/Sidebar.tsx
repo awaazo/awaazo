7f3ac462ef617f39b4f28989d67717f53588a153
Sheikh John <jonathan.githubofficial@gmail.com>
2024-01-04 10:40:25 -0500

    updated sidebar

--- frontend/components/shared/Sidebar.tsx
+++ frontend/components/shared/Sidebar.tsx
@@ -1,56 +1,80 @@
-import React from "react";
-import { useBreakpointValue, Box, VStack, Flex, Icon, Image, useMediaQuery } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Flex, Icon, Image, VStack } from "@chakra-ui/react";
+import Link from "next/link";
 import { FaHome, FaSearch } from "react-icons/fa";
+import { RiBookLine } from "react-icons/ri";
 import Logo from "../../public/logo_white.svg";
 import MyShelf from "../playlist/PlaylistSidebar";
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
+import CreatePlaylistModal from "../playlist/CreatePlaylistModal";
 
 const Sidebar = () => {
   const router = useRouter();
-  const [isLargerThanTablet] = useMediaQuery("(min-width: 48em)");
+  const [collapsed, setCollapsed] = useState(true);
+  const [reload, setReload] = useState(false);
+  const toggleCollapsed = () => setCollapsed(!collapsed);
 
-  const sidebarWidth = useBreakpointValue({ base: "100%", md: "60px", lg: "300px" });
-  const menuItems = [
-    { icon: FaHome, label: "Home", href: "/" },
-    { icon: FaSearch, label: "Explore", href: "/Explore/Search" },
-  ];
-
-  const SidebarItem = ({ icon, label, href }) => {
-    const isActive = router.pathname === href;
-    const displayLabel = isLargerThanTablet ? label : '';
-
-    return (
-      <Link href={href} passHref>
-        <Flex as="a" align="center" p="2" mb="1" borderRadius="md" color={isActive ? "brand.100" : "grey.700"} transition="color 0.4s ease-in-out" _hover={{ textDecoration: 'none', color: 'brand.100' }}>
-          <Icon as={icon} fontSize="xl" mr={3} />
-          <Box flex="1">{displayLabel}</Box>
-        </Flex>
-      </Link>
-    );
+  const onPlaylistAdded = () => {
+    setReload(!reload);
+  };
+  const handleModalClick = (event) => {
+    event.stopPropagation(); 
   };
 
   return (
-    <Box
-      bg="rgba(0, 0, 0, 0.3)"
-      backdropFilter="blur(35px)"
-      w={sidebarWidth}
-      h="100vh"
-      py={8} px={5}
-      position="static"
-      zIndex={10}
-      data-testid="navbar-component"
-    >
-      <Flex mb={10} justify="start" align="center">
-        <Image src={Logo.src} alt="Logo" mr={1} w="30px" />
+    <Box bg="rgba(0, 0, 0, 0.3)" backdropFilter="blur(35px)" w={collapsed ? "60px" : "300px"} h="100vh" py={8} px={collapsed ? 2 : 5} position="sticky" top="0" zIndex={10} display={{ base: "none", md: "block" }} transition="width 0.4s ease-in-out">
+      <Flex justify="center" align="center" mb={5}>
+        <Image src={Logo.src} alt="Logo" w="30px" />
       </Flex>
 
-      <VStack align="start" spacing={1}>
-        {menuItems.map(item => (
-          <SidebarItem key={item.label} {...item} />
-        ))}
+      <VStack align="left" spacing={1}>
+        <Box p={1} bg={"black"} rounded={"xl"} width={"100%"}>
+          {/* Home */}
+          <Link href="/" passHref>
+            <Flex as={Flex} align="center" p="2" mb="1" borderRadius="md" color={router.pathname === "/" ? "brand.100" : "grey.700"} transition="color 0.4s ease-in-out" _hover={{ textDecoration: "none", color: "brand.100" }}>
+              <Icon as={FaHome} fontSize="xl" mr={3} />
+              {!collapsed && (
+                <Box flex="1" fontWeight="bold">
+                  Home
+                </Box>
+              )}
+            </Flex>
+          </Link>
 
-        {isLargerThanTablet && <MyShelf />}
+          {/* Explore */}
+          <Link href="/Explore/Search" passHref>
+            <Flex as={Flex} align="center" p="2" mb="1" borderRadius="md" color={router.pathname === "/Explore/Search" ? "brand.100" : "grey.700"} transition="color 0.4s ease-in-out" _hover={{ textDecoration: "none", color: "brand.100" }}>
+              <Icon as={FaSearch} fontSize="xl" mr={3} />
+              {!collapsed && (
+                <Box flex="1" fontWeight="bold">
+                  Explore
+                </Box>
+              )}
+            </Flex>
+          </Link>
+        </Box>
+
+        <Box p={1} bg={"black"} rounded={"xl"} width={"100%"}>
+          {/* My Shelf */}
+          <Flex align="center" p="2" mb="1" borderRadius="md" color="grey.700" transition="color 0.4s ease-in-out" _hover={{ textDecoration: "none", color: "brand.100" }} onClick={toggleCollapsed}>
+          <Icon as={RiBookLine} fontSize="xl" mr={3} />
+          {!collapsed && (
+            <Box flex="1" fontWeight="bold">
+              My Shelf
+            </Box>
+          )}
+          {!collapsed && (
+            <Box onClick={handleModalClick}>
+              <CreatePlaylistModal onPlaylistAdded={onPlaylistAdded} />
+            </Box>
+          )}
+        </Flex>
+          {!collapsed && (
+            <Box overflowY="auto" maxHeight="calc(100vh - 350px)" w="100%">
+              <MyShelf />
+            </Box>
+          )}
+        </Box>
       </VStack>
     </Box>
   );
