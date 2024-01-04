7526cf2b242296cb2aae4dc2bbdab2a2344f366b
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2024-01-03 22:19:18 -0500

    Added transcript parsing, filtering on database with collection name

--- Python/ingest.py
+++ Python/ingest.py
@@ -3,7 +3,7 @@ from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
-
+import json
 import os
 os.environ["OPENAI_API_KEY"] = ""
 from langchain.chat_models import ChatOpenAI
@@ -11,6 +11,24 @@ llm = ChatOpenAI(model_name="gpt-3.5")
 
 # Data Ingestion
 from langchain.document_loaders import DirectoryLoader
+
+episode_id = "kanye_west_joe_rogan"
+
+# Load the JSON data from the file
+with open('./transcripts/transcript.json', 'r') as file:
+    data = json.load(file)
+
+# Extract the 'text' values from each dictionary in the list
+text_values = [entry['text'] for entry in data]
+
+# Combine the text values into a single string
+combined_text = '\n'.join(text_values)
+
+# Write the combined text to a .txt file
+with open(f'./transcripts/{episode_id}.txt', 'w') as output_file:
+    output_file.write(combined_text)
+print("Transcript parsed as text")
+
 
 transcript_loader = DirectoryLoader('./transcripts', glob="**/*.txt")
 
@@ -30,15 +48,17 @@ vectorstore = Chroma.from_documents(
     documents, 
     embeddings,
     persist_directory="./vectorstore",
+    collection_name=episode_id
     )
 vectorstore.persist()
 print("Vectorstore persisted")
+print("Vectostore collections: " + str(vectorstore._collection))
 
 print("Answering questions")
 
 general_system_template = r""" 
 Given a specific context, please give a short answer to the question. Kanye West is a rapper, producer, and fashion designer. You have context of him speaking with Joe Rogan in a transcript. 
-Pretend you are Joe Rogan answering the question as if you just finished interviewing Kanye West and someone asked you the question.
+Pretend you are Joe Rogan answering the question as if you just finished interviewing Kanye West and someone asked you the question. Make sure to act like Joe Rogan in your answer by using his tone and style of speaking.
  ----
 {context}
 ----
