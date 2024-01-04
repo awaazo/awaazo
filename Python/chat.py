13612c175599f73db20975dc45eb96b3b7d31440
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2024-01-04 00:23:54 -0500

    Added unique IDs, persistent database, overrite functionality, separated chat and loading to database scripts

---
+++ Python/chat.py
@@ -0,0 +1,34 @@
+from langchain.vectorstores import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+embedding = OpenAIEmbeddings()
+
+vectorstore = Chroma(embedding_function=embedding)
+
+general_system_template = r""" 
+Given a specific context, please give a short answer to the question. Kanye West is a rapper, producer, and fashion designer. You have context of him speaking with Joe Rogan in a transcript. 
+Pretend you are Joe Rogan answering the question as if you just finished interviewing Kanye West and someone asked you the question. Make sure to act like Joe Rogan in your answer by using his tone and style of speaking.
+ ----
+{context}
+----
+"""
+general_user_template = "Question:```{question}```"
+messages = [
+            SystemMessagePromptTemplate.from_template(general_system_template),
+            HumanMessagePromptTemplate.from_template(general_user_template)
+]
+qa_prompt = ChatPromptTemplate.from_messages( messages )
+
+qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.1), vectorstore.as_retriever(), combine_docs_chain_kwargs={"prompt": qa_prompt})
+
+response = qa({"question": "What is this podcast transcript about?", "chat_history": []})
+print("\nQuestion: " + response.get("question") + "\n\n" + response.get("answer"))
--- Python/ingest.py
+++ Python/ingest.py
@@ -1,16 +1,19 @@
-from langchain.embeddings.openai import OpenAIEmbeddings
+import uuid
 from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.chains import ConversationalRetrievalChain
-from langchain.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
+from langchain.document_loaders import DirectoryLoader
 import json
 import os
-os.environ["OPENAI_API_KEY"] = ""
+from dotenv import load_dotenv
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# Data Ingestion
 from langchain.chat_models import ChatOpenAI
 llm = ChatOpenAI(model_name="gpt-3.5")
 
-# Data Ingestion
-from langchain.document_loaders import DirectoryLoader
+embeddings = OpenAIEmbeddings()
 
 episode_id = "kanye_west_joe_rogan"
 
@@ -29,7 +32,6 @@ with open(f'./transcripts/{episode_id}.txt', 'w') as output_file:
     output_file.write(combined_text)
 print("Transcript parsed as text")
 
-
 transcript_loader = DirectoryLoader('./transcripts', glob="**/*.txt")
 
 loaders = [transcript_loader]
@@ -43,44 +45,24 @@ text_splitter = CharacterTextSplitter(chunk_size=1200, chunk_overlap=200)
 documents = text_splitter.split_documents(documents)
 print("Documents split into chunks")
 
-embeddings = OpenAIEmbeddings()
-vectorstore = Chroma.from_documents(
-    documents, 
-    embeddings,
-    persist_directory="./vectorstore",
-    collection_name=episode_id
+# Create a list of unique ids for each document based on the content
+ids = [str(uuid.uuid5(uuid.NAMESPACE_DNS, doc.page_content)) for doc in documents]
+unique_ids = list(set(ids))
+
+# Ensure that only docs that correspond to unique ids are kept
+seen_ids = set()
+unique_docs = [doc for doc, id in zip(documents, ids) if id not in seen_ids and (seen_ids.add(id) or True)]
+
+# Add the unique documents to your database
+vectorstoreDB = Chroma.from_documents(
+    unique_docs, 
+    embeddings, 
+    ids=unique_ids, 
+    persist_directory='vectorstore',
+    collection_name=episode_id,
     )
-vectorstore.persist()
+vectorstoreDB.persist()
 print("Vectorstore persisted")
-print("Vectostore collections: " + str(vectorstore._collection))
+print("Vectostore collections: " + str(vectorstoreDB._collection.count()))
 
-print("Answering questions")
-
-general_system_template = r""" 
-Given a specific context, please give a short answer to the question. Kanye West is a rapper, producer, and fashion designer. You have context of him speaking with Joe Rogan in a transcript. 
-Pretend you are Joe Rogan answering the question as if you just finished interviewing Kanye West and someone asked you the question. Make sure to act like Joe Rogan in your answer by using his tone and style of speaking.
- ----
-{context}
-----
-"""
-general_user_template = "Question:```{question}```"
-messages = [
-            SystemMessagePromptTemplate.from_template(general_system_template),
-            HumanMessagePromptTemplate.from_template(general_user_template)
-]
-qa_prompt = ChatPromptTemplate.from_messages( messages )
-
-# Initialise Langchain - Conversation Retrieval Chain
-qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.1), vectorstore.as_retriever(), combine_docs_chain_kwargs={"prompt": qa_prompt})
-
-response = qa({"question": "What is this podcast transcript about?", "chat_history": []})
-print("\nQuestion: " + response.get("question") + "\nAnswer: " + response.get("answer"))
-
-response = qa({"question": "Where in the podcast does he talk about his presidential run?", "chat_history": []})
-print("\nQuestion: " + response.get("question") + "\nAnswer: " + response.get("answer"))
-
-response = qa({"question": "What are Kanye's thoughts on religion?", "chat_history": []})
-print("\nQuestion: " + response.get("question") + "\nAnswer: " + response.get("answer"))
-
-response = qa({"question": "What did kanye mean when he mentioned Oprah Winfrey?", "chat_history": []})
-print("\nQuestion: " + response.get("question") + "\nAnswer: " + response.get("answer"))
+print("Run chat.py to use the vector db to answer questions")
