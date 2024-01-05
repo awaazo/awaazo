b202dc7d8af761adc5454a5dd061ae450ec426de
Sheikh John <jonathan.githubofficial@gmail.com>
2024-01-05 12:35:56 -0500

    updated the genre cards

--- frontend/components/cards/GenreCard.tsx
+++ frontend/components/cards/GenreCard.tsx
@@ -1,52 +1,28 @@
 import Link from "next/link";
-import { Box, Text, Image } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 
-const GenreCard = ({ genre, isHovered, onMouseEnter, onMouseLeave }) => {
-    const scale = isHovered ? "scale(1.1)" : "scale(0.95)";
-  
-    return (
-      <Link href={`/Explore/Genre/${genre.link}`} passHref>
-        <Box
-          key={genre.name}
-          h="100%"
-          borderRadius="1.2em"
-          overflow="hidden"
-          position="relative"
-          _hover={{
-            boxShadow: "xl",
-            cursor: "pointer",
-            transition: "all 0.4s ease-in-out",
-          }}
-          transform={scale}
-          transition="all 0.4s ease-in-out"
-          boxSizing="border-box"
-          outline={"2px solid rgba(255, 255, 255, .3)"}
-          onMouseEnter={() => onMouseEnter(genre.name)}
-          onMouseLeave={onMouseLeave}
-          zIndex={isHovered ? 1 : 0}
-        >
-          <Image
-            src={genre.image.src}
-            alt={`${genre.name} background`}
-            width="100%"
-            height="100%"
-            objectFit="cover"
-            opacity="0.8"
-          />
-          <Text
-            fontWeight="bold"
-            fontSize={["md", "lg", "xl"]}
-            position="absolute"
-            left="50%"
-            top="50%"
-            transform="translate(-50%, -50%)"
-            textShadow="2px 2px 4px #000000"
-            p={1}
-          >
-            {genre.name}
-          </Text>
-        </Box>
-      </Link>
-    );
-  };
-  export default GenreCard;
+const GenreCard = ({ genre, onMouseEnter, onMouseLeave }) => {
+  return (
+    <Link href={`/Explore/Genre/${genre.link}`} passHref>
+      <Box
+        h="180px"
+        w="180px"
+        borderRadius="30px"
+        overflow="hidden"
+        position="relative"
+        _hover={{
+          opacity: "0.7",
+          cursor: "pointer",
+          transition: "box-shadow 0.3s ease-in-out, opacity 0.5s ease-in-out",
+        }}
+        boxSizing="border-box"
+        onMouseEnter={() => onMouseEnter(genre.name)}
+        onMouseLeave={onMouseLeave}
+      >
+        <Image src={genre.image.src} alt={`${genre.name} background`} width="100%" height="100%" objectFit="cover" />
+      </Box>
+    </Link>
+  );
+};
+
+export default GenreCard;
--- frontend/components/home/ExploreGenres.tsx
+++ frontend/components/home/ExploreGenres.tsx
@@ -1,17 +1,17 @@
 import { Box, Text, Grid, Image } from "@chakra-ui/react";
 import { useState } from "react";
 import techImage from "../../styles/images/genres/tech.png";
-import educationImage from "../../styles/images/genres/ed.png";
+import medImage from "../../styles/images/genres/med.png";
 import comedyImage from "../../styles/images/genres/comedy.png";
 import politicsImage from "../../styles/images/genres/politics.png";
 import crimeImage from "../../styles/images/genres/crime.png";
-import otherImage from "../../styles/images/genres/other.jpg";
+import otherImage from "../../styles/images/genres/other.png";
 import GenreCard from "../cards/GenreCard"
 
 // Define the genres array with name, image, and link properties
 const genres = [
   { name: "Tech", image: techImage, link: "Technology" },
-  { name: "Education", image: educationImage, link: "Education" },
+  { name: "Medical", image: medImage, link: "Medical" },
   { name: "Comedy", image: comedyImage, link: "Comedy" },
   { name: "Politics", image: politicsImage, link: "Politics" },
   { name: "Crime", image: crimeImage, link: "Crime" },
@@ -24,14 +24,13 @@ const ExploreGenres = () => {
   return (
     <Box marginBottom="3em">
       <Grid
-        templateColumns={["repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(6, 1fr)"]}
+        templateColumns={["repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(9, 1fr)"]}
         gap={4}
       >
         {genres.map((genre) => (
           <GenreCard
             key={genre.name}
             genre={genre}
-            isHovered={hoveredGenre === genre.name}
             onMouseEnter={setHoveredGenre}
             onMouseLeave={() => setHoveredGenre(null)}
           />
