5c3bab7610595b6ef8fa7660eb477c0d86e8883a
Muhammad Mubashir <mubashirqbl@gmail.com>
2024-01-07 01:33:15 -0500

    Added Annotation Endpoints

---
+++ Backend/Backend/Controllers/AnnotationController.cs
@@ -0,0 +1,154 @@
+﻿using Azure.Core;
+using Backend.Controllers.Requests;
+using Backend.Models;
+using Backend.Services;
+using Backend.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("annotation")]
+    [Authorize]
+    public class AnnotationController : ControllerBase
+    {
+        private readonly IAnnotationService _annotationService;
+        private readonly IAuthService _authService;
+        private readonly ILogger _logger;
+
+        public AnnotationController(IAnnotationService annotationService, IAuthService authService, ILogger logger)
+        {
+            _annotationService = annotationService;
+            _authService = authService;
+            _logger = logger;
+        }
+        [HttpPost("{episodeId}/createAnnotation")]
+        public async Task<IActionResult> CreateAnnotation(Guid episodeId,GeneralAnnotationRequest annotationRequest)
+        {
+            try
+            {
+                // Log the message
+                _logger.LogDebug(@"Using the annotation\episodeId\createAnnotation Endpoint");
+
+                // Identify User from JWT Token
+                User? user = await _authService.IdentifyUserAsync(HttpContext);
+
+                // If User is not found, return 404
+                if (user == null)
+                    return NotFound("User does not exist.");
+                // Call the services Method
+                return await _annotationService.AddAnnotationToEpisodeAsync(user.Id,episodeId,annotationRequest) ? Ok("Successfully Created Annotation") : BadRequest("Database Error Occured");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+
+        [HttpPost("{episodeId}/createMediaLinkAnnotation")]
+        public async Task<IActionResult> CreateMediaLinkAnnotation(Guid episodeId, MediaLinkAnnotationRequest annotationRequest)
+        {
+            try
+            {
+                // Log the message
+                _logger.LogDebug(@"Using the annotation\episodeId\createMediaLinkAnnotation Endpoint");
+
+                // Identify User from JWT Token
+                User? user = await _authService.IdentifyUserAsync(HttpContext);
+
+                // If User is not found, return 404
+                if (user == null)
+                    return NotFound("User does not exist.");
+                // Call the services Method
+                return await _annotationService.AddMediaAnnotationToEpisodeAsync(user.Id, episodeId, annotationRequest) ? Ok("Successfully Created Annotation") : BadRequest("Database Error Occured");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+        [HttpPost("{episodeId}/createSponserAnnotation")]
+        public async Task<IActionResult> CreateSponserAnnotation(Guid episodeId, SponsershipAnnotationRequest annotationRequest)
+        {
+            try
+            {
+                // Log the message
+                _logger.LogDebug(@"Using the annotation\episodeId\createSponserAnnotation Endpoint");
+
+                // Identify User from JWT Token
+                User? user = await _authService.IdentifyUserAsync(HttpContext);
+
+                // If User is not found, return 404
+                if (user == null)
+                    return NotFound("User does not exist.");
+                // Call the services Method
+                return await _annotationService.AddSponsershipAnnotationToEpisodeAsync(user.Id, episodeId, annotationRequest) ? Ok("Successfully Created Annotation") : BadRequest("Database Error Occured");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+        [HttpGet("{episodeId}/getAnnotation")]
+        public async Task<IActionResult> GetAnnotation(Guid episodeId)
+        {
+            try
+            {
+                // Log the message
+                _logger.LogDebug(@"Using the annotation\episodeId\GetAnnotation Endpoint");
+
+                // Identify User from JWT Token
+                User? user = await _authService.IdentifyUserAsync(HttpContext);
+
+                // If User is not found, return 404
+                if (user == null)
+                    return NotFound("User does not exist.");
+                // Call the services Method
+                return Ok(await _annotationService.GetEpisodeAnnotationAsync(episodeId));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+        [HttpDelete("{annotationId}/delete")]
+        public async Task<IActionResult> DeleteAnnotation(Guid annotationId)
+        {
+            try
+            {
+                // Log the message
+                _logger.LogDebug(@"Using the annotation\annotationId\delete Endpoint");
+
+                // Identify User from JWT Token
+                User? user = await _authService.IdentifyUserAsync(HttpContext);
+
+                // If User is not found, return 404
+                if (user == null)
+                    return NotFound("User does not exist.");
+                // Call the services Method
+                return await _annotationService.DeleteAnnotationAsync(user.Id, annotationId) ? Ok("Successfully Deleted Annotation") : BadRequest("Database Error Occured");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ Backend/Backend/Services/AnnotationService.cs
@@ -0,0 +1,229 @@
+﻿using Backend.Controllers.Requests;
+using Backend.Controllers.Responses;
+using Backend.Infrastructure;
+using Backend.Models;
+using Backend.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using static Backend.Models.Annotation;
+using static Backend.Models.MediaLink;
+
+namespace Backend.Services
+{
+    public class AnnotationService : IAnnotationService
+    {
+        private readonly AppDbContext _db;
+        public AnnotationService(AppDbContext db) { 
+            _db = db;
+        }
+
+
+        /// <summary>
+        /// Create Link and Info Annotations
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="episodeId"></param>
+        /// <param name="annotationRequest"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+
+        public async Task<bool> AddAnnotationToEpisodeAsync(Guid userId, Guid episodeId, GeneralAnnotationRequest annotationRequest)
+        {
+           // Check if Episode Exist or Not
+            Episode epsiode = await _db.Episodes.Include(u => u.Podcast).FirstOrDefaultAsync(u => u.Id == episodeId) ?? throw new Exception("Episode Does Not Exist");
+           
+           // Check if Podcast Owner is same as the logged In user
+           if (epsiode.Podcast.PodcasterId != userId)
+           {
+                throw new Exception("Not authourized to Add Annotation");
+           }
+           
+           // Check if Time stamp is not more then the duration of episode
+           if (annotationRequest.Timestamp > epsiode.Duration)
+            {
+                throw new Exception("Not a valid Timestamp");
+            }
+
+           // Check if other Annotation Exist at the same time in the episode
+           if(await _db.Annotations.AnyAsync(u => u.EpisodeId == episodeId && u.Timestamp == annotationRequest.Timestamp) == true)
+            {
+                throw new Exception("Other annotation Exist on the same timestamp");
+
+           }
+           // Check if the Annotation Type Inputted is Link or Info
+            AnnotationType annotationType = GetAnnotationType(annotationRequest.AnnotationType);
+         
+           if (annotationType != AnnotationType.Info &&  annotationType != AnnotationType.Link)
+           {
+                // If not then throw an error to inform the user the other controller
+                throw new Exception("Use other controller to create Media-link and Sponsership Annotation");
+           }
+           // Save to the database
+            await _db.Annotations.AddAsync(new Annotation() { EpisodeId = episodeId, Timestamp = annotationRequest.Timestamp, Content = annotationRequest.Content,Type = annotationType });
+            
+            // Return a boolean
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+       /// <summary>
+       /// Saves Media Link Annotation to the database
+       /// </summary>
+       /// <param name="userId"></param>
+       /// <param name="episodeId"></param>
+       /// <param name="annotationRequest"></param>
+       /// <returns></returns>
+       /// <exception cref="Exception"></exception>
+        public async Task<bool> AddMediaAnnotationToEpisodeAsync(Guid userId, Guid episodeId, MediaLinkAnnotationRequest annotationRequest)
+        {
+            // Check if Episode Exist or Not
+            Episode epsiode = await _db.Episodes.Include(u => u.Podcast).FirstOrDefaultAsync(u => u.Id == episodeId) ?? throw new Exception("Episode Does Not Exist");
+
+            // Check if Podcast Owner is same as the logged In user
+            if (epsiode.Podcast.PodcasterId != userId)
+            {
+                throw new Exception("Not authourized to Add Annotation");
+            }
+
+            // Check if Time stamp is not more then the duration of episode
+            if (annotationRequest.Timestamp > epsiode.Duration)
+            {
+                throw new Exception("Not a valid Timestamp");
+            }
+
+            // Check if other Annotation Exist at the same time in the episode
+            if (await _db.Annotations.AnyAsync(u => u.EpisodeId == episodeId && u.Timestamp == annotationRequest.Timestamp) == true)
+            {
+                throw new Exception("Other annotation Exist on the same timestamp");
+
+            }
+
+            // Save the new Annotation
+            Guid AnnotationGuid = Guid.NewGuid();
+            Annotation annotation = new Annotation()
+            {
+                Id = AnnotationGuid,
+                EpisodeId = episodeId,
+                Timestamp = annotationRequest.Timestamp,
+                Content = annotationRequest.Content,
+                Type = AnnotationType.MediaLink
+            };
+
+            await _db.Annotations.AddAsync(annotation);
+
+            // Save the Media Link
+            MediaLink mediaLink = new MediaLink()
+            {
+                AnnotationId = AnnotationGuid,
+                Platform = getPlatformType(annotationRequest.PlatformType),
+                Url = annotationRequest.Url,
+
+            };
+
+            await _db.MediaLinks.AddAsync(mediaLink);
+
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+        /// <summary>
+        /// Create Sponsership Annotation 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="episodeId"></param>
+        /// <param name="annotationRequest"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<bool> AddSponsershipAnnotationToEpisodeAsync(Guid userId, Guid episodeId, SponsershipAnnotationRequest annotationRequest)
+        {
+
+            // Check if Episode Exist or Not
+            Episode epsiode = await _db.Episodes.Include(u => u.Podcast).FirstOrDefaultAsync(u => u.Id == episodeId) ?? throw new Exception("Episode Does Not Exist");
+
+            // Check if Podcast Owner is same as the logged In user
+            if (epsiode.Podcast.PodcasterId != userId)
+            {
+                throw new Exception("Not authourized to Add Annotation");
+            }
+
+            // Check if Time stamp is not more then the duration of episode
+            if (annotationRequest.Timestamp > epsiode.Duration)
+            {
+                throw new Exception("Not a valid Timestamp");
+            }
+
+            // Check if other Annotation Exist at the same time in the episode
+            if (await _db.Annotations.AnyAsync(u => u.EpisodeId == episodeId && u.Timestamp == annotationRequest.Timestamp) == true)
+            {
+                throw new Exception("Other annotation Exist on the same timestamp");
+
+            }
+
+            // Save the new Annotation
+            Guid AnnotationGuid = Guid.NewGuid();
+            Annotation annotation = new Annotation()
+            {
+                Id = AnnotationGuid,
+                EpisodeId = episodeId,
+                Timestamp = annotationRequest.Timestamp,
+                Content = annotationRequest.Content,
+                Type = AnnotationType.Sponsorship
+            };
+
+            await _db.Annotations.AddAsync(annotation);
+
+            // Save the Sponser details to the DB
+            Sponsor sponsor = new Sponsor()
+            {
+                AnnotationId = AnnotationGuid,
+                Name = annotationRequest.Name,
+                Website = annotationRequest.Website
+            };
+            await _db.Sponsors.AddAsync(sponsor);
+
+            return await _db.SaveChangesAsync() > 0;
+
+        }
+        /// <summary>
+        /// Fetches Annotation for Each episode
+        /// </summary>
+        /// <param name="episodeId"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<List<AnnotationResponse>> GetEpisodeAnnotationAsync(Guid episodeId)
+        {
+            // Check if Episode Exist or Not
+            Episode epsiode = await _db.Episodes.Include(u => u.Podcast).FirstOrDefaultAsync(u => u.Id == episodeId) ?? throw new Exception("Episode Does Not Exist");
+
+            // Fetch Annotation for the Episode
+            List<AnnotationResponse> annotation = await _db.Annotations.Include(u => u.MediaLink).Include(u => u.Sponsorship).Where(u => u.EpisodeId == episodeId).Select(u => new AnnotationResponse(u)).ToListAsync();
+
+            // Return the list
+            return annotation;
+        }
+        /// <summary>
+        /// Delete the Annotation
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="annotationId"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<bool> DeleteAnnotationAsync(Guid userId,Guid annotationId)
+        {
+            // Check if annotation Exist
+            Annotation annotation = await _db.Annotations.Include(u => u.Episode).ThenInclude(e => e.Podcast).FirstOrDefaultAsync(u => u.Id == annotationId) ?? throw new Exception("Annotation Does Not exist");
+
+            // Check if the logged In user is the owner of the podcast
+
+            if(annotation.Episode.Podcast.PodcasterId != userId)
+            {
+                throw new Exception("Not Authorized to Delete the Annotation");
+
+            }
+            // Delete the Annotation
+            _db.Annotations.Remove(annotation);
+
+            return await _db.SaveChangesAsync() > 0;
+        }
+
+    }
+
+
+}
