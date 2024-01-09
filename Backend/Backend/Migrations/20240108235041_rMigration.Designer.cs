7b5b14e16f2c83960f52388de04f0261a89a9cbf
sFranchetto <31280516+sFranchetto@users.noreply.github.com>
2024-01-08 19:02:53 -0500

    migrations added

--- Backend/Backend/Migrations/20240108235041_rMigration.Designer.cs
+++ Backend/Backend/Migrations/20240108235041_rMigration.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20231222231459_firstMigration")]
-    partial class firstMigration
+    [Migration("20240108235041_rMigration")]
+    partial class rMigration
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
