4d3060e372887ef5000215da54cd938060b7c65e
Radwanbaba13 <radwangbaba@gmail.com>
2024-01-19 16:50:32 -0500

    Fixed Migration Issue causing profile service to fail

--- backend/Backend/Migrations/20240119214337_xMigration.Designer.cs
+++ backend/Backend/Migrations/20240119214337_xMigration.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20240116164534_firstMigration")]
-    partial class firstMigration
+    [Migration("20240119214337_xMigration")]
+    partial class xMigration
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
