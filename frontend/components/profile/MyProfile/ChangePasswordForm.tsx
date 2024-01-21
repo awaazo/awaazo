4d4ef405051bb3e3df6a292fda13f06f9427a579
Sheikh John <jonathan.githubofficial@gmail.com>
2024-01-21 03:27:49 -0500

    code optimization

--- frontend/components/profile/MyProfile/ChangePasswordForm.tsx
+++ frontend/components/profile/MyProfile/ChangePasswordForm.tsx
@@ -1,27 +1,17 @@
 import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Stack,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Button, FormControl, Heading, Input, InputGroup, Stack, Text } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import UserProfileHelper from "../../../helpers/UserProfileHelper";
 import { ChangePasswordRequest } from "../../../utilities/Requests";
 
 const ChangePasswordForm: React.FC = () => {
-    const [changePasswordError, setChangePasswordError] = useState<string | null>("");
-    const [oldPassword, setOldPassword] = useState<string | null>("");
-    const [newPassword, setNewPassword] = useState<string | null>("");
-    const [confirmNewPassword, setConfirmPassword] = useState<string | null>("");
-    const [showOldPassword, setShowOldPassword] = useState(false);
-    const [showNewPassword, setShowNewPassword] = useState(false);
-    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [changePasswordError, setChangePasswordError] = useState<string | null>("");
+  const [oldPassword, setOldPassword] = useState<string | null>("");
+  const [newPassword, setNewPassword] = useState<string | null>("");
+  const [confirmNewPassword, setConfirmPassword] = useState<string | null>("");
+  const [showOldPassword] = useState(false);
+  const [showNewPassword] = useState(false);
+  const [showConfirmPassword] = useState(false);
 
   const handleChangePassword = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -54,7 +44,7 @@ const ChangePasswordForm: React.FC = () => {
       setChangePasswordError("An error occurred while changing Passwords...");
     }
   };
-  
+
   const handlePasswordChange = (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
 
@@ -71,116 +61,30 @@ const ChangePasswordForm: React.FC = () => {
       default:
         break;
     }
-
     setChangePasswordError(null);
   };
 
-  const handleTogglePasswordVisibility = (field: string) => () => {
-    switch (field) {
-      case "oldPassword":
-        setShowOldPassword(!showOldPassword);
-        break;
-      case "newPassword":
-        setShowNewPassword(!showNewPassword);
-        break;
-      case "confirmNewPassword":
-        setShowConfirmPassword(!showConfirmPassword);
-        break;
-      default:
-        break;
-    }
-  };
 
   return (
-    <Box p={6} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+    <Box p={3} display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+      <Heading fontSize="2xl" marginBottom="1.5em">
+        Change Password
+      </Heading>
       <form onSubmit={handleChangePassword}>
         <Stack spacing={6} align={"center"}>
           {changePasswordError && <Text color="red.500">{changePasswordError}</Text>}
-
-          <FormControl >
-            <FormLabel>Current Password</FormLabel>
-            <InputGroup>
-              <Input
-                type={showOldPassword ? "text" : "password"}
-                id="currentPassword"
-                placeholder="Enter current password"
-                onChange={handlePasswordChange("oldPassword")}
-                borderRadius="0.8em"
-              />
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={handleTogglePasswordVisibility("oldPassword")}>
-                  {showOldPassword ? <ViewOffIcon /> : <ViewIcon />}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
+        
 
           <FormControl>
-            <FormLabel>New Password</FormLabel>
-            <InputGroup>
-              <Input
-                type={showNewPassword ? "text" : "password"}
-                id="newPassword"
-                placeholder="Enter new password"
-                onChange={handlePasswordChange("newPassword")}
-                borderRadius="0.8em"
-              />
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={handleTogglePasswordVisibility("newPassword")}>
-                  {showNewPassword ? <ViewOffIcon /> : <ViewIcon />}
-                </Button>
-              </InputRightElement>
+            <InputGroup flexDirection="column" >
+              <Input type={showOldPassword ? "text" : "password"} id="currentPassword" placeholder="Enter current password" onChange={handlePasswordChange("oldPassword")} borderRadius="1em" marginBottom={4} />
+              <Input type={showNewPassword ? "text" : "password"} id="newPassword" placeholder="Enter new password" onChange={handlePasswordChange("newPassword")} borderRadius="1em" marginBottom={4}/>
+              <Input type={showConfirmPassword ? "text" : "password"} id="confirmNewPassword" placeholder="Confirm new password" onChange={handlePasswordChange("confirmNewPassword")} borderRadius="1em" marginBottom={4}/>
             </InputGroup>
           </FormControl>
 
-          <FormControl>
-            <FormLabel>Confirm New Password</FormLabel>
-            <InputGroup>
-              <Input
-                type={showConfirmPassword ? "text" : "password"}
-                id="confirmNewPassword"
-                placeholder="Confirm new password"
-                onChange={handlePasswordChange("confirmNewPassword")}
-                borderRadius="0.8em"
-              />
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={handleTogglePasswordVisibility("confirmNewPassword")}>
-                  {showConfirmPassword ? <ViewOffIcon /> : <ViewIcon />}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </FormControl>
-
-          <Button
-            type="submit"
-            fontSize="md"
-            borderRadius="full"
-            minWidth="200px"
-            color="white"
-            marginTop="15px"
-            marginBottom="10px"
-            padding="20px"
-            outline="1px solid rgba(255, 255, 255, 0.6)"
-            style={{
-              background: "linear-gradient(45deg, #007BFF, #3F60D9, #5E43BA, #7C26A5, #9A0A90)",
-              backgroundSize: "300% 300%",
-              animation: "Gradient 10s infinite linear",
-            }}
-          >
+          <Button type="submit" variant="gradient">
             Change Password
-            <style jsx>{`
-              @keyframes Gradient {
-                0% {
-                  background-position: 100% 0%;
-                }
-                50% {
-                  background-position: 0% 100%;
-                }
-                100% {
-                  background-position: 100% 0%;
-                }
-              }
-            `}</style>
           </Button>
         </Stack>
       </form>
--- frontend/components/social/commentComponent.tsx
+++ frontend/components/social/commentComponent.tsx
@@ -1,34 +1,8 @@
 import { useState, useEffect } from "react";
 import SocialHelper from "../../helpers/SocialHelper";
 import PodcastHelper from "../../helpers/PodcastHelper";
-import {
-  Button,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  Textarea,
-  VStack,
-  useDisclosure,
-  Icon,
-  Avatar,
-  Text,
-  HStack,
-  Box,
-  Tooltip,
-  Input,
-  useBreakpointValue,
-  IconButton,
-} from "@chakra-ui/react";
-import {
-  FaComments,
-  FaClock,
-  FaPaperPlane,
-  FaTrash,
-  FaReply,
-} from "react-icons/fa";
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Textarea, VStack, useDisclosure, Icon, Avatar, Text, HStack, Box, Tooltip, Input, useBreakpointValue, IconButton } from "@chakra-ui/react";
+import { FaComments, FaClock, FaPaperPlane, FaTrash, FaReply } from "react-icons/fa";
 import { Comment, User } from "../../utilities/Interfaces";
 import AuthHelper from "../../helpers/AuthHelper";
 import LikeComponent from "./likeComponent";
@@ -56,23 +30,19 @@ const CommentComponent = ({ episodeIdOrCommentId, initialComments }) => {
             setUser(response.userMenuInfo);
           }
         });
-        const response = await PodcastHelper.getEpisodeById(
-          episodeIdOrCommentId,
-        );
+        const response = await PodcastHelper.getEpisodeById(episodeIdOrCommentId);
         if (response.status === 200) {
           if (response.episode) {
             // Transform the comments to match our format
-            const transformedComments = response.episode.comments.map(
-              (comment) => ({
-                id: comment.id,
-                episodeId: comment.episodeId,
-                user: comment.user,
-                dateCreated: new Date(comment.dateCreated),
-                text: comment.text,
-                likes: comment.likes,
-                replies: comment.replies,
-              }),
-            );
+            const transformedComments = response.episode.comments.map((comment) => ({
+              id: comment.id,
+              episodeId: comment.episodeId,
+              user: comment.user,
+              dateCreated: new Date(comment.dateCreated),
+              text: comment.text,
+              likes: comment.likes,
+              replies: comment.replies,
+            }));
             setComments(transformedComments);
           }
         } else {
@@ -86,10 +56,7 @@ const CommentComponent = ({ episodeIdOrCommentId, initialComments }) => {
   // Add a new comment
   const handleAddComment = async () => {
     if (newComment.trim()) {
-      const response = await SocialHelper.postEpisodeComment(
-        newComment,
-        episodeIdOrCommentId,
-      );
+      const response = await SocialHelper.postEpisodeComment(newComment, episodeIdOrCommentId);
       if (response.status === 200) {
         // Update the UI to reflect the new comment
         setNoOfComments((noOfComments) => noOfComments + 1);
@@ -109,10 +76,7 @@ const CommentComponent = ({ episodeIdOrCommentId, initialComments }) => {
     const commentId = comment.id;
     const updatedComments = [...comments];
 
-    const response = await SocialHelper.postEpisodeComment(
-      replyTexts[index],
-      commentId,
-    );
+    const response = await SocialHelper.postEpisodeComment(replyTexts[index], commentId);
     if (response.status === 200) {
       // Update the UI to reflect the new reply
       setReplyChange((replyChange) => replyChange + 1);
@@ -146,55 +110,23 @@ const CommentComponent = ({ episodeIdOrCommentId, initialComments }) => {
   return (
     <>
       <Tooltip label="Comment" aria-label="Comment tooltip">
-        <Button
-          p={2}
-          m={1}
-          leftIcon={<Icon as={FaComments} />}
-          onClick={onOpen}
-          variant={"ghost"}
-          data-cy={`playerbar-comment-button`}
-        >
+        <Button p={2} m={1} leftIcon={<Icon as={FaComments} />} onClick={onOpen} variant={"ghost"} data-cy={`playerbar-comment-button`}>
           {noOfComments}
         </Button>
       </Tooltip>
 
       <Modal isOpen={isOpen} onClose={onClose} size="xl">
         <ModalOverlay />
-        <ModalContent
-          backdropFilter="blur(40px)"
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          alignSelf={"center"}
-          padding={"2em"}
-          backgroundColor="rgba(255, 255, 255, 0.1)"
-          borderRadius={"2em"}
-          outlineColor="rgba(255, 255, 255, 0.25)"
-        >
+        <ModalContent alignItems="center">
           <ModalHeader fontWeight={"light"} fontSize={"1.5em"}>
             Comments
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <VStack
-              spacing={5}
-              align="start"
-              width={isMobile ? "350px" : "500px"}
-              height="400px"
-              overflowY="auto"
-            >
+            <VStack spacing={5} align="start" width={isMobile ? "350px" : "500px"} height="400px" overflowY="auto">
               {comments && comments.length > 0 ? (
                 comments.map((comment, index) => (
-                  <Box
-                    key={index}
-                    p={3}
-                    borderRadius="md"
-                    boxShadow="sm"
-                    bg={"gray.550"}
-                    _hover={{ transition: "0.3s" }}
-                    width="100%"
-                  >
+                  <Box key={index} p={3} borderRadius="md" boxShadow="sm" bg={"gray.550"} _hover={{ transition: "0.3s" }} width="100%">
                     <HStack spacing={5}>
                       <Avatar src={comment.user.avatarUrl} />
                       <VStack align="start" spacing={1} flex="1">
@@ -208,21 +140,9 @@ const CommentComponent = ({ episodeIdOrCommentId, initialComments }) => {
                             </Text>
                           </VStack>
                           <HStack spacing={2}>
-                            <LikeComponent
-                              episodeOrCommentId={comment.id}
-                              initialLikes={comment.likes}
-                            />
+                            <LikeComponent episodeOrCommentId={comment.id} initialLikes={comment.likes} />
                             {user.id === comment.user.id ? (
-                              <IconButton
-                                icon={<Icon as={FaTrash} />}
-                                variant={"ghost"}
-                                aria-label="Delete Comment"
-                                data-cy={`delete-comment-id:`}
-                                onClick={() =>
-                                  handleDeleteComment(comment.id, true)
-                                }
-                                size="md"
-                              />
+                              <IconButton icon={<Icon as={FaTrash} />} variant={"ghost"} aria-label="Delete Comment" data-cy={`delete-comment-id:`} onClick={() => handleDeleteComment(comment.id, true)} size="md" />
                             ) : null}
                           </HStack>
                         </HStack>
@@ -235,49 +155,25 @@ const CommentComponent = ({ episodeIdOrCommentId, initialComments }) => {
                       </Text>
                     </HStack>
                     <VStack align="start" spacing={2} mt={3} pl={8}>
-                      {comment.replies
-                        .slice(0, numRepliesToShow)
-                        .map((reply, index) => (
-                          <Box
-                            key={index}
-                            bg="gray.650"
-                            p={2}
-                            borderRadius="md"
-                            width="100%"
-                          >
-                            <HStack spacing={5} flex={1}>
-                              <Avatar src={reply.user.avatarUrl} />
-                              <VStack align="start" spacing={1} flex="1">
-                                <Text fontWeight="bold">
-                                  {reply.user.username}:
-                                </Text>
-                                <Text whiteSpace="pre-line">
-                                  {reply.text.replace(/(.{40})/g, "$1\n")}
-                                </Text>
-                              </VStack>
-                              <HStack spacing={2}>
-                                {user.id === reply.user.id ? (
-                                  <IconButton
-                                    icon={<Icon as={FaTrash} />}
-                                    variant={"ghost"}
-                                    aria-label="Delete Reply"
-                                    onClick={() =>
-                                      handleDeleteComment(reply.id, false)
-                                    }
-                                    size="sm"
-                                  />
-                                ) : null}
-                              </HStack>
-                            </HStack>
+                      {comment.replies.slice(0, numRepliesToShow).map((reply, index) => (
+                        <Box key={index} bg="gray.650" p={2} borderRadius="md" width="100%">
+                          <HStack spacing={5} flex={1}>
+                            <Avatar src={reply.user.avatarUrl} />
+                            <VStack align="start" spacing={1} flex="1">
+                              <Text fontWeight="bold">{reply.user.username}:</Text>
+                              <Text whiteSpace="pre-line">{reply.text.replace(/(.{40})/g, "$1\n")}</Text>
+                            </VStack>
+                            <HStack spacing={2}>{user.id === reply.user.id ? <IconButton icon={<Icon as={FaTrash} />} variant={"ghost"} aria-label="Delete Reply" onClick={() => handleDeleteComment(reply.id, false)} size="sm" /> : null}</HStack>
+                          </HStack>
 
-                            <HStack spacing={1} p={2} borderRadius="md">
-                              <Icon as={FaClock} color="gray.500" />
-                              <Text fontSize="xs" color="gray.500">
-                                {new Date(reply.dateCreated).toLocaleString()}
-                              </Text>
-                            </HStack>
-                          </Box>
-                        ))}
+                          <HStack spacing={1} p={2} borderRadius="md">
+                            <Icon as={FaClock} color="gray.500" />
+                            <Text fontSize="xs" color="gray.500">
+                              {new Date(reply.dateCreated).toLocaleString()}
+                            </Text>
+                          </HStack>
+                        </Box>
+                      ))}
                       <Box mt={2} width="100%">
                         <HStack spacing={2}>
                           <Input
@@ -290,17 +186,8 @@ const CommentComponent = ({ episodeIdOrCommentId, initialComments }) => {
                               setReplyTexts(updatedReplyTexts);
                             }}
                           />
-                          <Tooltip
-                            label="Reply to this comment"
-                            aria-label="Reply tooltip"
-                          >
-                            <IconButton
-                              icon={<Icon as={FaReply} />}
-                              onClick={() => handleReply(index)}
-                              aria-label="Reply to Comment"
-                              size="sm"
-                              data-cy={`reply-button`}
-                            />
+                          <Tooltip label="Reply to this comment" aria-label="Reply tooltip">
+                            <IconButton icon={<Icon as={FaReply} />} onClick={() => handleReply(index)} aria-label="Reply to Comment" size="sm" data-cy={`reply-button`} />
                           </Tooltip>
                         </HStack>
                       </Box>
@@ -314,32 +201,8 @@ const CommentComponent = ({ episodeIdOrCommentId, initialComments }) => {
               )}
             </VStack>
             <VStack position={"relative"}>
-              <Textarea
-                value={newComment}
-                onChange={(e) => setNewComment(e.target.value)}
-                placeholder="Add a comment..."
-                borderRadius={"1em"}
-              />
-              <Button
-                leftIcon={<Icon as={FaPaperPlane} />}
-                colorScheme="blue"
-                onClick={handleAddComment}
-                zIndex="1"
-                fontSize="md"
-                borderRadius={"full"}
-                minWidth={"10em"}
-                color={"white"}
-                marginTop={"15px"}
-                marginBottom={"10px"}
-                padding={"20px"}
-                outline={"1px solid rgba(255, 255, 255, 0.6)"}
-                style={{
-                  background:
-                    "linear-gradient(45deg, #007BFF, #3F60D9, #5E43BA, #7C26A5, #9A0A90)",
-                  backgroundSize: "300% 300%",
-                  animation: "Gradient 10s infinite linear",
-                }}
-              >
+              <Textarea value={newComment} onChange={(e) => setNewComment(e.target.value)} placeholder="Add a comment..." borderRadius={"1em"} />
+              <Button leftIcon={<Icon as={FaPaperPlane} />} onClick={handleAddComment} zIndex="1" variant="gradient">
                 Add Comment
               </Button>
             </VStack>
