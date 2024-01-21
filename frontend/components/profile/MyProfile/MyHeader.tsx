10d5d783b9fa2d6f7ab69744938bad71b4fd64d2
Sheikh John <jonathan.githubofficial@gmail.com>
2024-01-21 03:29:04 -0500

    simple optimization and routing fixes

--- frontend/components/profile/MyProfile/MyHeader.tsx
+++ frontend/components/profile/MyProfile/MyHeader.tsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import { Avatar, Heading, Text, VStack, Link, IconButton, Divider, Flex, Box, HStack, useColorModeValue, useBreakpointValue, Button } from "@chakra-ui/react";
 import { UserProfile } from "../../../utilities/Interfaces";
 import { useSession } from "next-auth/react";
-// Here we have used react-icons package for the iconS
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+import { FaXTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa6";
+import { FiEdit2 } from "react-icons/fi";
 import router from "next/router";
 import UserProfileHelper from "../../../helpers/UserProfileHelper";
 import Subscriptions from "./MySubscriptions";
@@ -14,35 +15,13 @@ const iconProps = {
   isRound: true,
 };
 
-const socials = [
-  {
-    url: "https://github.com/",
-    label: "Github Account",
-    type: "gray",
-    icon: <FaGithub />,
-  },
-  {
-    url: "https://twitter.com/",
-    label: "Twitter Account",
-    type: "twitter",
-    icon: <FaTwitter />,
-  },
-
-  {
-    url: "https://linkedin.com/",
-    label: "LinkedIn Account",
-    type: "linkedin",
-    icon: <FaLinkedin />,
-  },
-];
-
 export default function Header() {
   const { data: session } = useSession();
   const [profile, setProfile] = useState<UserProfile>(null);
 
   const isMobile = useBreakpointValue({ base: true, md: false });
 
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false); // New state to track login status
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   useEffect(() => {
     UserProfileHelper.profileGetRequest()
@@ -60,7 +39,22 @@ export default function Header() {
     <>
       <VStack spacing={4} px={2} alignItems={{ base: "center", sm: "flex-start" }} marginBottom={"2em"} ml={isMobile ? "25px" : "0px"}>
         <HStack>
-          <Avatar boxShadow="xl" style={{ width: "150px", height: "150px" }} src={profile?.avatarUrl} />
+          <Box position="relative">
+            <Avatar boxShadow="xl" width="7em" height="7em" src={profile?.avatarUrl} />
+            <IconButton
+              aria-label="Edit Profile"
+              icon={<FiEdit2 />}
+              position="absolute"
+              bottom={6}
+              right={6}
+              transform={"translate(50%, 50%)"}
+              variant="ghost"
+              rounded="3xl"
+              onClick={() => router.push("/profile/EditProfile")}
+              backgroundColor="brand.100"
+              _hover={{ bg: "brand.300" }}
+            />
+          </Box>
           <VStack align="start" spacing={1}>
             <Heading textAlign={{ base: "center", sm: "left" }} margin="0 auto" fontSize={{ base: "2rem", sm: "2.5rem" }}>
               {profile?.displayName}
@@ -81,21 +75,9 @@ export default function Header() {
         <Flex alignItems="center" justify="center" w="100%">
           <Box textAlign="center">
             <IconButton as={Link} isExternal href={profile?.githubUrl} aria-label={"Github Account"} colorScheme={"gray"} rounded="full" icon={<FaGithub />} {...iconProps} />
-            <IconButton as={Link} isExternal href={profile?.twitterUrl} aria-label={"Twitter Account"} colorScheme={"gray"} rounded="full" icon={<FaTwitter />} {...iconProps} />
-            <IconButton as={Link} isExternal href={profile?.linkedInUrl} aria-label={"Linkedin Account"} colorScheme={"gray"} rounded="full" icon={<FaLinkedin />} {...iconProps} />
+            <IconButton as={Link} isExternal href={profile?.twitterUrl} aria-label={"Twitter Account"} colorScheme={"gray"} rounded="full" icon={<FaXTwitter />} {...iconProps} />
+            <IconButton as={Link} isExternal href={profile?.linkedInUrl} aria-label={"Linkedin Account"} colorScheme={"gray"} rounded="full" icon={<FaLinkedinIn />} {...iconProps} />
           </Box>
-
-          <Button
-            rounded={"full"}
-            style={{
-              marginLeft: "1em",
-            }}
-            onClick={() => {
-              router.push("/profile/EditProfile");
-            }}
-          >
-            <Text>Edit Profile</Text>
-          </Button>
         </Flex>
       </VStack>
     </>
