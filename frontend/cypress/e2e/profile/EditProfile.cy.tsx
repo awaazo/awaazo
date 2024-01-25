23c7ad685d326f858704ff6dc9fd4fa01ae24e51
sFranchetto <31280516+sFranchetto@users.noreply.github.com>
2024-01-25 16:54:05 -0500

    Added longer timeout to fix failing test

--- frontend/cypress/e2e/profile/EditProfile.cy.tsx
+++ frontend/cypress/e2e/profile/EditProfile.cy.tsx
@@ -11,7 +11,7 @@ describe('EditProfile', () => {
     //User should be abble to edit profile and the changes should be reflected immediately
     it('Should successfully edit profile', function(){
         cy.edit_profile(paths.profile_picture, 'NewUsername', 'NewBio','https://twitter.com/newTwitterUsername','https://linkedin.com/newLinkedInUsername','https://github.com/newGitHub');
-        cy.contains('NewUsername', { timeout: 5000 });
+        cy.contains('NewUsername', { timeout: 15000 });
         cy.contains('NewBio', { timeout: 5000 });
     });
 
