4942f3f36b6b86072c6fd7de1a728a16c7d9e65c
Radwanbaba13 <radwangbaba@gmail.com>
2024-01-26 16:31:57 -0500

    Updated Card Styles

--- frontend/components/cards/EpisodeCard.tsx
+++ frontend/components/cards/EpisodeCard.tsx
@@ -43,15 +43,13 @@ import PlaylistHelper from "../../helpers/PlaylistHelper";
 
 import ShareComponent from "../social/shareComponent";
 import { convertTime } from "../../utilities/commonUtils";
+import EpisodeMenu from "./EpisodeMenu";
 
 // Component to display an episode
 const EpisodeCard = ({ episode, inPlaylist, playlistId }) => {
   const { dispatch } = usePlayer();
   const toast = useToast();
 
-  const [isShareModalOpen, setIsShareModalOpen] = useState(false);
-  const onShareModalClose = () => setIsShareModalOpen(false);
-  const onShareModalOpen = () => setIsShareModalOpen(true);
   // Handle click on episode
   const handleEpisodeClick = () => {
     dispatch({
@@ -60,78 +58,7 @@ const EpisodeCard = ({ episode, inPlaylist, playlistId }) => {
     });
   };
 
-  // Handle click play next on episode
-  const handleEpisodeNextClick = () => {
-    dispatch({
-      type: "ADD_NEXT_QUEUE",
-      payload: episode,
-    });
-  };
-
-  // Handle click play later on episode
-  const handleEpisodeLaterClick = () => {
-    dispatch({
-      type: "ADD_LATER_QUEUE",
-      payload: episode,
-    });
-  };
-
   const isMobile = useBreakpointValue({ base: true, md: false });
-
-  // State to track whether the menu is open or not
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // Handle menu open and close
-  const handleMenuToggle = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  // Handle menu item click and show toast notification
-  const handleMenuItemClick = (action) => {
-    if (action === "playNext") {
-      handleEpisodeNextClick();
-      toast({
-        title: "Episode added to queue",
-        status: "info",
-        position: "bottom-right",
-        duration: 3000,
-        isClosable: true,
-      });
-    } else if (action === "playLater") {
-      handleEpisodeLaterClick();
-      toast({
-        title: "Episode added to queue",
-        status: "info",
-        position: "bottom-right",
-        duration: 3000,
-        isClosable: true,
-      });
-    }
-  };
-
-  // Add to Playlist implementation
-  const [isAddToPlaylistModalOpen, setIsAddToPlaylistModalOpen] =
-    useState(false);
-
-  // Handlers to open/close the modal:
-  const handleAddToPlaylistMenuToggle = () => {
-    setIsAddToPlaylistModalOpen(!isAddToPlaylistModalOpen);
-  };
-
-  // handles Remove from Playlist
-  const handleRemovePlaylistMenuToggle = async () => {
-    const request = [episode.id];
-    const response = await PlaylistHelper.playlistRemoveEpisodeRequest(
-      request,
-      playlistId,
-    );
-    console.log(response);
-    if (response.status == 200) {
-      window.location.reload();
-    } else {
-      console.log("Playlist cannot be deleted");
-    }
-  };
 
   return (
     <Flex
@@ -143,19 +70,8 @@ const EpisodeCard = ({ episode, inPlaylist, playlistId }) => {
       bg={"rgba(0, 0, 0, 0.2)"}
       backdropFilter="blur(4px)"
       boxShadow="sm"
-      style={{ cursor: "pointer", transition: "transform 0.3s" }}
-      onMouseOver={(e) => {
-        // Check if the menu is open before applying the transform
-        if (!isMenuOpen) {
-          e.currentTarget.style.opacity = "0.8";
-        }
-      }}
-      onMouseOut={(e) => {
-        // Check if the menu is open before applying the transform
-        if (!isMenuOpen) {
-          e.currentTarget.style.opacity = "1";
-        }
-      }}
+      style={{ cursor: "pointer" }}
+      onClick={isMobile ? handleEpisodeClick : null}
       onDoubleClick={handleEpisodeClick}
     >
       <Box position="relative" mr={5} onClick={() => handleEpisodeClick()}>
@@ -222,142 +138,12 @@ const EpisodeCard = ({ episode, inPlaylist, playlistId }) => {
             showCount={true}
           />
         </Box>
-        {/* Episode Options Menu */}
-        <Box style={{ position: "relative", zIndex: 1000 }}>
-          <Menu isOpen={isMenuOpen} onClose={handleMenuToggle}>
-            <MenuButton
-              as={IconButton}
-              aria-label="Options"
-              icon={<IoIosMore />}
-              variant="ghost"
-              fontSize="20px"
-              ml={1}
-              mt={1}
-              _hover={{ boxShadow: "lg" }}
-              onClick={handleMenuToggle}
-              data-cy={`2-dots-episode-card`}
-            />
-            <MenuList
-              style={{
-                backgroundColor: "rgba(50, 50, 50, 0.8)",
-                backdropFilter: "blur(4px)",
-              }}
-            >
-              <MenuItem
-                _hover={{
-                  backgroundColor: "rgba(255, 255, 255, 0.4)",
-                  fontWeight: "bold",
-                }}
-                style={{ backgroundColor: "transparent" }}
-                onClick={handleAddToPlaylistMenuToggle}
-              >
-                Add to Playlist{" "}
-                <MdOutlinePlaylistAdd
-                  size={24}
-                  style={{ marginLeft: "auto", color: "white" }}
-                />
-              </MenuItem>
-              {inPlaylist && (
-                <MenuItem
-                  _hover={{
-                    backgroundColor: "rgba(255, 255, 255, 0.4)",
-                    fontWeight: "bold",
-                  }}
-                  style={{ backgroundColor: "transparent", color: "red" }}
-                  onClick={() => handleRemovePlaylistMenuToggle()}
-                >
-                  Remove from Playlist{" "}
-                  <MdDelete
-                    size={24}
-                    style={{ marginLeft: "auto", color: "red" }}
-                  />
-                </MenuItem>
-              )}
-              <MenuDivider />
-              <MenuItem
-                onClick={() => handleEpisodeClick()}
-                _hover={{
-                  backgroundColor: "rgba(255, 255, 255, 0.8)",
-                  fontWeight: "bold",
-                }}
-                style={{
-                  backgroundColor: "transparent",
-                }}
-              >
-                Play "{episode.episodeName}"{" "}
-                <BsPlayFill
-                  size="20px"
-                  style={{ marginLeft: "auto", color: "white" }}
-                />
-              </MenuItem>
-              <MenuItem
-                onClick={() => handleMenuItemClick("playNext")}
-                _hover={{
-                  backgroundColor: "rgba(255, 255, 255, 0.8)",
-                  fontWeight: "bold",
-                }}
-                style={{
-                  backgroundColor: "transparent",
-                }}
-              >
-                Play Next{" "}
-                <TbPlayerTrackNextFilled
-                  size="18px"
-                  style={{ marginLeft: "auto", color: "white" }}
-                />
-              </MenuItem>
-              <MenuItem
-                onClick={() => handleMenuItemClick("playLater")}
-                _hover={{
-                  backgroundColor: "rgba(255, 255, 255, 0.8)",
-                  fontWeight: "bold",
-                }}
-                style={{
-                  backgroundColor: "transparent",
-                }}
-              >
-                Play Later{" "}
-                <BsFillSkipForwardFill
-                  size="18px"
-                  style={{ marginLeft: "auto", color: "white" }}
-                />
-              </MenuItem>
-              <MenuDivider />
-              <MenuItem
-                onClick={onShareModalOpen}
-                _hover={{
-                  backgroundColor: "rgba(255, 255, 255, 0.8)",
-                  fontWeight: "bold",
-                }}
-                style={{
-                  backgroundColor: "transparent",
-                }}
-              >
-                Share{" "}
-                <MdIosShare
-                  size="20px"
-                  style={{ marginLeft: "auto", color: "white" }}
-                />
-              </MenuItem>
-            </MenuList>
-          </Menu>
-        </Box>
-        <Modal isOpen={isShareModalOpen} onClose={onShareModalClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Share this Episode</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <ShareComponent content={episode} contentType="episode" />
-            </ModalBody>
-          </ModalContent>
-        </Modal>
+        <EpisodeMenu
+          episode={episode}
+          inPlaylist={inPlaylist}
+          playlistId={playlistId}
+        />
       </Flex>
-      <AddToPlaylistModal
-        isOpen={isAddToPlaylistModalOpen}
-        onClose={() => setIsAddToPlaylistModalOpen(false)}
-        episode={episode}
-      />
     </Flex>
   );
 };
