c59cbb34d3b0c9f071f0604ff0214e3c23080709
Radwanbaba13 <radwangbaba@gmail.com>
2024-01-31 21:02:15 -0500

    Revert Updates to Recently Uploaded File

--- frontend/components/home/RecentlyUploaded.tsx
+++ frontend/components/home/RecentlyUploaded.tsx
@@ -7,7 +7,6 @@ import {
   useBreakpointValue,
   Spinner,
   SimpleGrid,
-  Flex,
 } from "@chakra-ui/react";
 import { Podcast } from "../../utilities/Interfaces";
 import PodcastHelper from "../../helpers/PodcastHelper";
@@ -41,23 +40,38 @@ const RecentlyUploaded: React.FC = () => {
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
-    <Box>
+    <VStack spacing={4} align="stretch">
       {isLoading ? (
         <Spinner size="xl" />
       ) : error ? (
         <Text color="red.500">{error}</Text>
-      ) : podcasts && podcasts.length > 0 ? (
-        <Flex flexWrap="wrap">
-          {podcasts.map((podcast) =>
-            podcast.episodes.map((episode) => (
-              <PodcastTicket key={episode.id} episode={episode} />
-            )),
-          )}
-        </Flex>
       ) : (
-        <Text>No episodes available</Text>
+        <HStack
+          spacing={4}
+          overflowX="auto"
+          css={{
+            "&::-webkit-scrollbar": {
+              display: "none",
+            },
+          }}
+        >
+          <SimpleGrid
+            columns={{ base: 3, sm: 4, md: 5, lg: 6, xl: 7 }}
+            spacing={5}
+          >
+            {podcasts && podcasts.length > 0 ? (
+              podcasts.map((podcast) =>
+                podcast.episodes.map((episode) => (
+                  <PodcastTicket key={episode.id} episode={episode} />
+                )),
+              )
+            ) : (
+              <Text>No episodes available</Text>
+            )}
+          </SimpleGrid>
+        </HStack>
       )}
-    </Box>
+    </VStack>
   );
 };
 
