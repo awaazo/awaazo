8cb7be091b2332490dcb9baae81f87a4d9a2be14
Sheikh John <jonathan.githubofficial@gmail.com>
2024-02-08 21:01:28 -0500

    migrations fix

--- Backend/Backend/Migrations/20240209015950_firstMigration.Designer.cs
+++ Backend/Backend/Migrations/20240209015950_firstMigration.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20240209001923_firstMigration")]
+    [Migration("20240209015950_firstMigration")]
     partial class firstMigration
     {
         /// <inheritdoc />
