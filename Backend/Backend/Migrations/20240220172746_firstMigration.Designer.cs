35b4cfde6c7b37beaa188bc04b2e1ad0e11a95b3
Mathieu Trudeau <mathieutrudeau11@gmail.com>
2024-02-20 12:27:58 -0500

    Update Migrations

--- Backend/Backend/Migrations/20240220172746_firstMigration.Designer.cs
+++ Backend/Backend/Migrations/20240220172746_firstMigration.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20240213030752_addedSoftDeleteAttrib")]
-    partial class addedSoftDeleteAttrib
+    [Migration("20240220172746_firstMigration")]
+    partial class firstMigration
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -872,6 +872,46 @@ namespace Backend.Migrations
                     b.HasIndex("FollowerId");
 
                     b.ToTable("UserFollows");
+                });
+
+            modelBuilder.Entity("Backend.Models.stats.AdminEmailLog", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<Guid>("AdminUserId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("Body")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<bool>("BodyIsHtml")
+                        .HasColumnType("bit");
+
+                    b.Property<DateTime>("CreatedAt")
+                        .HasColumnType("datetime2");
+
+                    b.Property<bool>("Delivered")
+                        .HasColumnType("bit");
+
+                    b.Property<string>("ErrorMessage")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Subject")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<Guid>("ToUserId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<DateTime>("UpdatedAt")
+                        .HasColumnType("datetime2");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("AdminEmailLogs");
                 });
 
             modelBuilder.Entity("Backend.Models.Annotation", b =>
