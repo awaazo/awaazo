8a483447dc5280e7494bd12ff03593336b76f000
Shadi <51339220+shadijiha@users.noreply.github.com>
2024-02-28 01:29:20 -0500

    Create ReportController.cs

---
+++ Backend/Backend/Controllers/ReportController.cs
@@ -0,0 +1,33 @@
+﻿using Backend.Infrastructure;
+using Backend.Models;
+using Backend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[Controller]
+[Authorize]
+public class ReportController : ControllerBase
+{
+    private readonly ReportService _service;
+    private readonly ILogger<ReportController> _logger;
+    public ReportController(ReportService service, ILogger<ReportController> logger) {
+        _service = service;
+        _logger = logger;
+    }
+    
+    [HttpPost("report")]
+    public async Task<IActionResult> Report([FromBody] Report report) {
+        try {
+            this.LogDebugControllerAPICall(_logger);
+            await _service.Report(report);
+            return Ok();
+        }
+        catch (Exception e) {
+            this.LogErrorAPICall(_logger, e);
+            return BadRequest(e.Message);
+        }
+    }
+
+}
