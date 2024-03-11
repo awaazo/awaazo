b6dff5e9fd1b8aca7c920611268c7a93f8d287ff
sFranchetto <31280516+sFranchetto@users.noreply.github.com>
2024-03-11 13:31:08 -0400

    Mobile episode upload done

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
       "cypress/e2e/**/MobileLogout.cy.tsx",
       "cypress/e2e/**/MobileEditProfile.cy.tsx",
       "cypress/e2e/**/MobileChangePassword.cy.tsx",
-      "cypress/e2e/**/MobilePodcast.cy.tsx"
+      "cypress/e2e/**/MobilePodcast.cy.tsx",
+      "cypress/e2e/**/MobileEpisode.cy.tsx"
     ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
