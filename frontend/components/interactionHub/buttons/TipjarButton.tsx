be28ae8b65f92c4d6ce5bf52ef197f394da7f049
Muhammad Mubashir <mubashirqbl@gmail.com>
2024-03-28 01:06:17 -0400

    Fixed tip

--- frontend/components/interactionHub/buttons/TipjarButton.tsx
+++ frontend/components/interactionHub/buttons/TipjarButton.tsx
@@ -1,14 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Tooltip, Icon } from "@chakra-ui/react";
 import { usePanel } from "../../../utilities/PanelContext";
 import { PiCurrencyDollarSimpleFill } from 'react-icons/pi';
 import AuthPrompt from "../../auth/AuthPrompt";
 import AuthHelper from "../../../helpers/AuthHelper";
+import { useRouter } from "next/router";
 
 const TipjarButton = ({ episodeId , totalPoint }) => {
   const { dispatch } = usePanel();
+  const router = useRouter()
+  const { success, failure } = router.query
   const [showLoginPrompt, setShowLoginPrompt] = useState(false);
 
+
+  useEffect(() => {
+    const checkQueryParameter = async () => {
+      if (success) {
+       
+        try {
+          dispatch({ type: "OPEN_PANEL", payload: "Tipjar" });
+          dispatch({ type: "SET_EPISODE_ID", payload: episodeId });
+
+        } catch (error) {
+          console.error('Error While Confirming the Payment', error)
+        }
+      } else if (failure) {
+        dispatch({ type: "OPEN_PANEL", payload: "Tipjar" });
+        dispatch({ type: "SET_EPISODE_ID", payload: episodeId });
+      }
+    }
+
+    checkQueryParameter()
+  }, [success, failure])
   const handleClick = async () => {
     try {
       const response = await AuthHelper.authMeRequest();
@@ -27,7 +50,8 @@ const TipjarButton = ({ episodeId , totalPoint }) => {
     <>
       <Tooltip label="Tipjar" aria-label="Tipjar">
         <Button padding={"0px"} variant={"ghost"} onClick={handleClick}>
-          <Icon as={PiCurrencyDollarSimpleFill} size="20px" />
+          <Icon as={PiCurrencyDollarSimpleFill} size="80px" />
+          {totalPoint}
         </Button>
       </Tooltip>
       {showLoginPrompt && (
