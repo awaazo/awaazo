9e2252ff6914c2f97041a05d013eca947f5e5993
Radwanbaba13 <radwangbaba@gmail.com>
2024-03-28 21:51:58 -0400

    Addition of Admin Email Logs

--- frontend/pages/Admin/EmailLogs.tsx
+++ frontend/pages/Admin/EmailLogs.tsx
@@ -1,14 +1,100 @@
-import React from 'react'
-import { Flex, Box } from '@chakra-ui/react'
+// EmailLogsPage.js
+import React, { useState, useEffect } from 'react'
+import { Flex, Box, Text } from '@chakra-ui/react'
 import AdminSidebar from '../../components/admin/AdminSidebar'
+import EmailLogs from '../../components/admin/EmailLogs'
+import UserProfileHelper from '../../helpers/UserProfileHelper'
+import AdminHelper from '../../helpers/AdminHelper'
 
-const EmailLogsPage = (props) => {
+const EmailLogsPage = () => {
+  const [emails, setEmails] = useState([])
+  const [selectedEmail, setSelectedEmail] = useState(null)
+
+  useEffect(() => {
+    fetchEmails()
+  }, [])
+
+  const fetchEmails = async () => {
+    try {
+      const response = await AdminHelper.adminGetLogs(0)
+      const emailData = response.emails || []
+      const populatedEmails = await Promise.all(
+        emailData.map(async (email) => {
+          const fromUser = await getUsername(email.adminUserId)
+          const toUser = await getUsername(email.toUserId)
+          return {
+            ...email,
+            from: fromUser,
+            to: toUser,
+          }
+        })
+      )
+      setEmails(populatedEmails)
+      if (populatedEmails.length > 0) {
+        setSelectedEmail(populatedEmails[0])
+      }
+    } catch (error) {
+      console.error('Error fetching emails:', error)
+    }
+  }
+
+  const getUsername = async (userId) => {
+    const res = await UserProfileHelper.profileGetByIdRequest(userId)
+    const user = res.userProfileByID
+    if (!user) {
+      return 'Deleted User'
+    }
+    return user.username
+  }
+
   return (
     <Flex maxHeight={'80vh'}>
       <AdminSidebar />
 
-      <Box flex="1" p="4">
-        <h1>Email Logs</h1>
+      <Box flex="1" p="4" ml={'15px'}>
+        <Text fontSize={'32px'} fontWeight={'bold'} mb={'15px'}>
+          Email Logs
+        </Text>
+        <Flex height={'85vh'}>
+          <EmailLogs emails={emails} setSelectedEmail={setSelectedEmail} selectedEmail={selectedEmail} />
+          {selectedEmail && (
+            <Box flexBasis="60%" ml="15px" overflow="auto" bg="rgba(129, 137, 144, 0.1)" borderRadius="20px" p="15px">
+              <Box borderRadius="10px" p="4">
+                <Box borderRadius="md" p="4" mt="2" display="flex" justifyContent="space-between" alignItems="flex-start">
+                  <Box>
+                    <Text fontSize="18px" fontWeight="bold" display="inline">
+                      From:{' '}
+                    </Text>
+                    <Text fontSize="18px" display="inline">
+                      {selectedEmail.from}
+                    </Text>
+                    <br />
+                    <Text fontSize="18px" fontWeight="bold" display="inline">
+                      To:{' '}
+                    </Text>
+                    <Text fontSize="18px" display="inline">
+                      {selectedEmail.to}
+                    </Text>
+                    <br />
+                    <Text fontSize="18px" fontWeight="bold" display="inline">
+                      Subject:{' '}
+                    </Text>
+                    <Text fontSize="18px" display="inline">
+                      {selectedEmail.subject}
+                    </Text>
+                    <br />
+                  </Box>
+                  <Text fontSize="16px" color="az.greyish">
+                    Sent at {new Date(selectedEmail.createdAt).toLocaleString()}
+                  </Text>
+                </Box>
+                <Text fontSize="16px" mt="30px" color="az.greyish">
+                  {selectedEmail.body}
+                </Text>
+              </Box>
+            </Box>
+          )}
+        </Flex>
       </Box>
     </Flex>
   )
--- frontend/types/Interfaces.tsx
+++ frontend/types/Interfaces.tsx
@@ -1,271 +1,273 @@
 export interface Podcast {
-  id: string;
-  coverArtUrl: string;
-  name: string;
-  podcasterId: string;
-  podcaster: string;
-  description: string;
-  tags: string[];
-  isExplicit: boolean;
-  type: "real" | "ai-generated";
-  creationDate: Date;
-  episodes: Episode[];
-  averageRating?: number;
-  monthlyListeners: number;
-  totalPodcastPoints: number;
-  highlights: Highlight[];
+  id: string
+  coverArtUrl: string
+  name: string
+  podcasterId: string
+  podcaster: string
+  description: string
+  tags: string[]
+  isExplicit: boolean
+  type: 'real' | 'ai-generated'
+  creationDate: Date
+  episodes: Episode[]
+  averageRating?: number
+  monthlyListeners: number
+  totalPodcastPoints: number
+  highlights: Highlight[]
 }
 
 export interface Episode {
-  id: string;
-  podcastId: string;
-  podcastName: string;
-  thumbnailUrl: string;
-  episodeName: string;
-  description: string;
-  duration: number;
-  releaseDate: Date;
-  isExplicit?: boolean;
-  playCount?: number;
-  isTranscriptionReady: boolean;
-  transcriptionStatus: string;
+  id: string
+  podcastId: string
+  podcastName: string
+  thumbnailUrl: string
+  episodeName: string
+  description: string
+  duration: number
+  releaseDate: Date
+  isExplicit?: boolean
+  playCount?: number
+  isTranscriptionReady: boolean
+  transcriptionStatus: string
   likes: {
-    count: number;
-    isLiked: boolean;
-  };
-  bookmarks?: Bookmark[];
-  sections?: Section[];
-  annotations: Annotation[];
-  sponsors: Sponsor[];
-  transcript?:Transcript[];
-  totalPoints:number;
-  highlights: Highlight[];
+    count: number
+    isLiked: boolean
+  }
+  bookmarks?: Bookmark[]
+  sections?: Section[]
+  annotations: Annotation[]
+  sponsors: Sponsor[]
+  transcript?: Transcript[]
+  totalPoints: number
+  highlights: Highlight[]
 }
 
 export interface Highlight {
-  highlightId: string;
-  id: string;
-  podcastId: string;
-  podcastName: string;
-  episodeId: string;
-  episodeName: string;
-  thumbnailUrl: string;
-  highlightName: string;
-  description: string;
-  duration: number;
-  releaseDate: Date;
-  isExplicit: boolean;
-  playCount: number;
+  highlightId: string
+  id: string
+  podcastId: string
+  podcastName: string
+  episodeId: string
+  episodeName: string
+  thumbnailUrl: string
+  highlightName: string
+  description: string
+  duration: number
+  releaseDate: Date
+  isExplicit: boolean
+  playCount: number
   likes: {
-    count: number;
-    isLiked: boolean;
-  };
-  comments: number;
-  bookmarks: number;
-  totalPoints: number;
-  annotations: Annotation[];
-  sponsors: Sponsor[];
-  transcript?:Transcript[];
-  Title: string;
-  Description: string;
+    count: number
+    isLiked: boolean
+  }
+  comments: number
+  bookmarks: number
+  totalPoints: number
+  annotations: Annotation[]
+  sponsors: Sponsor[]
+  transcript?: Transcript[]
+  Title: string
+  Description: string
 }
 
 export interface Playlist {
-  id: string;
-  name: string;
-  user: User;
-  description: string;
-  privacy: string;
-  isHandledByUser: boolean;
-  numberOfEpisodes: number;
-  duration: number;
-  createdAt: Date;
-  updatedAt: Date;
-  playlistEpisodes: Episode[];
-  coverArt: string;
-  lastUpdated: string;
+  id: string
+  name: string
+  user: User
+  description: string
+  privacy: string
+  isHandledByUser: boolean
+  numberOfEpisodes: number
+  duration: number
+  createdAt: Date
+  updatedAt: Date
+  playlistEpisodes: Episode[]
+  coverArt: string
+  lastUpdated: string
 }
 
 export interface Section {
-  id: string;
-  episodeId: string;
-  title: string;
-  start: number;
-  end: number;
+  id: string
+  episodeId: string
+  title: string
+  start: number
+  end: number
 }
 
 export interface Comment {
-  id: string;
-  user: User;
-  episodeId: string;
-  text: string;
-  dateCreated: Date;
-  likes: number;
-  noOfReplies: number;
+  id: string
+  user: User
+  episodeId: string
+  text: string
+  dateCreated: Date
+  likes: number
+  noOfReplies: number
 }
 
 export interface Like {
-  userId: number;
-  commentId?: number;
-  episodeId?: string;
+  userId: number
+  commentId?: number
+  episodeId?: string
 }
 
 export interface Reply {
-  id: number;
-  user: User;
-  text: string;
-  dateCreated: Date;
-  likes: Like[];
+  id: number
+  user: User
+  text: string
+  dateCreated: Date
+  likes: Like[]
 }
 
 export interface Annotation {
-  id: string;
-  timestamp: number;
-  content: string;
-  type: "link" | "info" | "sponsorship" | "media-link";
-  sponsorship?: Sponsor;
-  mediaLink?: MediaLink;
+  id: string
+  timestamp: number
+  content: string
+  type: 'link' | 'info' | 'sponsorship' | 'media-link'
+  sponsorship?: Sponsor
+  mediaLink?: MediaLink
 }
 
 export interface Sponsor {
-  id: string;
-  name: string;
-  website: string;
+  id: string
+  name: string
+  website: string
 }
 
 export interface MediaLink {
-  platform: "YouTube" | "Spotify" | "Apple Music";
-  url: string;
+  platform: 'YouTube' | 'Spotify' | 'Apple Music'
+  url: string
 }
 
 export interface Bookmark {
-  id: string;
-  title: string;
-  note?: string;
-  time: number;
+  id: string
+  title: string
+  note?: string
+  time: number
 }
 
 export interface WatchHistory {
-  listenPosition: number;
+  listenPosition: number
 }
 
 export interface Transcript {
-  id: number;
-  seek: number;
-  start: number;
-  end: number;
-  text: string;
-  speaker: string;
+  id: number
+  seek: number
+  start: number
+  end: number
+  text: string
+  speaker: string
   words: {
-    start: number;
-    end: number;
-    word: string;
-    score: number;
-    speaker: string;
-  }[];
+    start: number
+    end: number
+    word: string
+    score: number
+    speaker: string
+  }[]
 }
 
 export interface Metrics {
-  totalEpisodesLikes: number;
-  mostLikedEpisode: string;
-  totalTimeWatched: number;
-  totalPlayCount: number;
-  mostPlayedEpisode: string;
-  totalCommentsCount: number;
-  mostCommentedOnEpisode: string;
-  mostLikedComment: string;
+  totalEpisodesLikes: number
+  mostLikedEpisode: string
+  totalTimeWatched: number
+  totalPlayCount: number
+  mostPlayedEpisode: string
+  totalCommentsCount: number
+  mostCommentedOnEpisode: string
+  mostLikedComment: string
 }
 
 export interface User {
-  id: string;
-  username: string;
-  displayName: string;
-  email: string;
-  passwordHash: string;
-  salt: string;
-  avatarUrl: string;
-  interests: string[];
-  dateOfBirth: Date;
-  gender: "male" | "female" | "other" | "prefer not to say";
-  isPodcaster: boolean;
-  podcasts?: Podcast[];
-  isAdmin: boolean;
+  id: string
+  username: string
+  displayName: string
+  email: string
+  passwordHash: string
+  salt: string
+  avatarUrl: string
+  interests: string[]
+  dateOfBirth: Date
+  gender: 'male' | 'female' | 'other' | 'prefer not to say'
+  isPodcaster: boolean
+  podcasts?: Podcast[]
+  isAdmin: boolean
+  deletedAt: Date
+  deletedBy: string
 }
 
 export interface PodcastFollow {
-  userId: string;
-  podcastId: string;
+  userId: string
+  podcastId: string
 }
 
 export interface UserFollow {
-  followerId: string;
-  followingId: string;
+  followerId: string
+  followingId: string
 }
 
 export interface Subscription {
-  userId: string;
-  type: "basic" | "premium";
-  startDate: Date;
-  endDate: Date;
+  userId: string
+  type: 'basic' | 'premium'
+  startDate: Date
+  endDate: Date
 }
 
 export interface PodcastRating {
-  id: string;
-  userId: string;
-  podcastId: string;
-  rating: number;
-  timestamp: Date;
+  id: string
+  userId: string
+  podcastId: string
+  rating: number
+  timestamp: Date
 }
 
 export interface UserEpisodeInteraction {
-  id: string;
-  userId: string;
-  episodeId: string;
-  hasListened: boolean;
-  lastListenedPosition?: number;
-  dateListened?: Date;
+  id: string
+  userId: string
+  episodeId: string
+  hasListened: boolean
+  lastListenedPosition?: number
+  dateListened?: Date
 }
 
 /**
  * User fields related to their profile.
  */
 export interface UserProfile {
-  id: string;
-  avatarUrl: string;
-  email: string;
-  displayName: string;
-  username: string;
-  bio: string;
-  interests: string[];
-  twitterUrl?: string;
-  githubUrl?: string;
-  linkedInUrl?: string;
-  websiteUrl?: string;
-  dateOfBirth: string;
-  gender: string;
+  id: string
+  avatarUrl: string
+  email: string
+  displayName: string
+  username: string
+  bio: string
+  interests: string[]
+  twitterUrl?: string
+  githubUrl?: string
+  linkedInUrl?: string
+  websiteUrl?: string
+  dateOfBirth: string
+  gender: string
 }
 
 export interface userProfileByID {
-  podcasts: Podcast[];
-  email: string;
-  displayName: string;
-  bio: string;
-  interests: string[];
-  twitterUrl?: string;
-  githubUrl?: string;
-  linkedInUrl?: string;
-  websiteUrl?: string;
-  dateOfBirth: string;
-  gender: string;
-  id: string;
-  avatarUrl: string;
-  username: string;
+  podcasts: Podcast[]
+  email: string
+  displayName: string
+  bio: string
+  interests: string[]
+  twitterUrl?: string
+  githubUrl?: string
+  linkedInUrl?: string
+  websiteUrl?: string
+  dateOfBirth: string
+  gender: string
+  id: string
+  avatarUrl: string
+  username: string
 }
 
 export interface UserMenuInfo {
-  id: string;
-  username: string;
-  avatarUrl: string;
+  id: string
+  username: string
+  avatarUrl: string
 }
 
 /**
@@ -273,77 +275,97 @@ export interface UserMenuInfo {
  * */
 
 export interface Notification {
-  id: string;
-  title: string;
-  message: string;
-  link: string;
-  isRead: boolean;
-  media: string;
-  type: string;
-  createdAt: Date;
+  id: string
+  title: string
+  message: string
+  link: string
+  isRead: boolean
+  media: string
+  type: string
+  createdAt: Date
 }
 
 export interface MySubscriptions {
-  id: string;
-  name: string;
-  description: string;
-  coverArtUrl: string;
-  podcasterId: string;
-  tags: string[];
-  isExplicit: boolean;
-  type: "real" | "ai-generated";
-  episodes: Episode[];
-  averageRating?: number;
-  totalRatings?: number;
-  ratings?: PodcastRating[];
+  id: string
+  name: string
+  description: string
+  coverArtUrl: string
+  podcasterId: string
+  tags: string[]
+  isExplicit: boolean
+  type: 'real' | 'ai-generated'
+  episodes: Episode[]
+  averageRating?: number
+  totalRatings?: number
+  ratings?: PodcastRating[]
 }
 
 export interface Chatbot {
-  id: string;
-  userId: string;
-  episodeId: string;
-  message: string;
-  isPrompt: boolean;
-  username: string;
-  avatarUrl: string;
-  sentAt: Date;
+  id: string
+  userId: string
+  episodeId: string
+  message: string
+  isPrompt: boolean
+  username: string
+  avatarUrl: string
+  sentAt: Date
 }
 
 export interface Transcript {
-  episodeId: string;
-  status: string;
-  lines: TranscriptLine[];
+  episodeId: string
+  status: string
+  lines: TranscriptLine[]
 }
 
 export interface TranscriptLine {
-  id: number;
-  seek: number;
-  start: number;
-  end: number;
-  text: string;
-  speaker: string;
-  words: TranscriptWord[];
+  id: number
+  seek: number
+  start: number
+  end: number
+  text: string
+  speaker: string
+  words: TranscriptWord[]
 }
 
 export interface TranscriptWord {
-  start: number;
-  end: number;
-  word: string;
-  score: number;
-  speaker: string;
+  start: number
+  end: number
+  word: string
+  score: number
+  speaker: string
 }
 
-export interface Transaction{
-  amount:number,
-  username : string,
-  date : Date,
-  userId :string,
-  senderId : string,
-  senderName : string,
-  type : string
+export interface Transaction {
+  amount: number
+  username: string
+  date: Date
+  userId: string
+  senderId: string
+  senderName: string
+  type: string
 }
 
 export interface Balance {
-  amount: number,
-  date : string
+  amount: number
+  date: string
+}
+
+export interface Email {
+  delivered: boolean
+  adminUserId: string
+  toUserId: string
+  subject: string
+  body: string
+  createdAt: Date
+}
+
+export interface Report {
+  id: string
+  targetEntityName: string
+  targetId: string
+  reportedBy: string
+  reason: string
+  status: number
+  deletedAt: Date
+  deletedBy: string
 }
