fbfaebbc1ee053762c68716c48a94c35aa76d19c
Sheikh John <jonathan.githubofficial@gmail.com>
2024-03-29 06:32:48 -0400

    split the custom Tabs into its own component

--- frontend/components/home/Home.tsx
+++ frontend/components/home/Home.tsx
@@ -1,64 +1,23 @@
-import React, { useState } from 'react'
-import { Box, Tab, TabList, TabPanel, TabPanels, Tabs, Icon, Container, HStack, Text } from '@chakra-ui/react'
-import RecentlyUploaded from './RecentlyUploaded'
-import ForYou from './ForYou'
-import HighLights from '../highlights/Highlights'
-import { AwaazoA } from '../../public/icons'
+import React from 'react';
+import { Box } from '@chakra-ui/react'
+import CustomTabs from '../assets/CustomTabs';
+import RecentlyUploaded from './RecentlyUploaded';
+import ForYou from './ForYou';
+import HighLights from '../highlights/Highlights';
+
 
 const Home = () => {
-  const [tabIndex, setTabIndex] = useState(0)
-  const handleTabsChange = (index) => {
-    setTabIndex(index)
-  }
-
-  const iconStyle = (isCurrentTab) => ({
-    boxSize: '0.7em',
-    color: 'az.red',
-    mb: 3,
-    mr: 0.5,
-    animation: isCurrentTab ? `pop 0.7s ease-in-out` : 'none',
-  })
+  const tabItems = [
+    { label: 'For You', component: <ForYou /> },
+    { label: 'Recently Uploaded', component: <RecentlyUploaded /> },
+    { label: 'Highlights', component: <HighLights />},
+  ];
 
   return (
     <Box px={['1em', '2em', '4em']} minH="100vh">
-      <Tabs index={tabIndex} onChange={handleTabsChange} isFitted variant="withIconOnSelected">
-        <TabList mb="1em">
-          <HStack spacing="1em">
-            <Tab justifyContent="center">
-              <Box display="flex" flexDirection="row" alignItems="Left" justifyContent="left">
-                {tabIndex === 0 && <Icon as={AwaazoA} {...iconStyle(tabIndex === 0)} />}
-                <Text whiteSpace="nowrap">For You</Text>
-              </Box>
-            </Tab>
-            <Tab>
-              <Box display="flex" flexDirection="row" alignItems="center" justifyContent="center">
-                {tabIndex === 1 && <Icon as={AwaazoA} {...iconStyle(tabIndex === 1)} />}
-                <Text whiteSpace="nowrap">Recently Uploaded</Text>
-              </Box>
-            </Tab>
-            <Tab>
-              <Box display="flex" flexDirection="row" alignItems="center" justifyContent="center">
-                {tabIndex === 2 && <Icon as={AwaazoA} {...iconStyle(tabIndex === 2)} />}
-                <Text whiteSpace="nowrap">Highlights</Text>
-              </Box>
-            </Tab>
-          </HStack>
-        </TabList>
-
-        <TabPanels>
-          <TabPanel>
-            <ForYou />
-          </TabPanel>
-          <TabPanel>
-            <RecentlyUploaded />
-          </TabPanel>
-          <TabPanel>
-            <HighLights />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      <CustomTabs tabItems={tabItems} />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
