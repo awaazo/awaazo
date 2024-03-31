3456a6aec229364957e102917a4e5a666d233563
Radwanbaba13 <radwangbaba@gmail.com>
2024-03-30 21:56:43 -0400

    Added Analytics Tab to CreatorHub

--- frontend/components/CreatorHub/MyPodcast.tsx
+++ frontend/components/CreatorHub/MyPodcast.tsx
@@ -1,15 +1,27 @@
-import { Box, List, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
-import PodcastInfo from "./PodcastInfo";
-import AddEpisode from "./AddEpisode";
+import { Box, List, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import PodcastInfo from './PodcastInfo'
+import AddEpisode from './AddEpisode'
+import Analytics from './Analytics'
 
 export default function MyPodcast({ podcastId }) {
   return (
     <>
-      <Box p={4} mt={"2em"} borderWidth="1px" borderRadius="1em" padding={"1.5em"} bg="rgba(255, 255, 255, 0.05)" backdropFilter={"blur(50px)"} dropShadow={" 0px 4px 4px rgba(0, 0, 0, 0.35)"} minHeight={"200px"}>
-        <Tabs isFitted width={"100%"}>
-          <TabList mb="1em" width={"30%"}>
+      <Box
+        p={4}
+        mt={'2em'}
+        borderWidth="1px"
+        borderRadius="1em"
+        padding={'1.5em'}
+        bg="rgba(255, 255, 255, 0.05)"
+        backdropFilter={'blur(50px)'}
+        dropShadow={' 0px 4px 4px rgba(0, 0, 0, 0.35)'}
+        minHeight={'200px'}
+      >
+        <Tabs isFitted width={'100%'}>
+          <TabList mb="1em" width={'30%'}>
             <Tab>Podcast Info</Tab>
             <Tab data-cy={`add-episode-tab`}>Add Episode</Tab>
+            <Tab>Analytics</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
@@ -22,9 +34,12 @@ export default function MyPodcast({ podcastId }) {
                 <AddEpisode podcastId={podcastId} />
               </List>
             </TabPanel>
+            <TabPanel>
+              <Analytics podcastId={podcastId} />
+            </TabPanel>
           </TabPanels>
         </Tabs>
       </Box>
     </>
-  );
+  )
 }
