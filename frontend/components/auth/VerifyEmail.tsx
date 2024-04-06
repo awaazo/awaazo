5c7afcc5705b70cff66650163c508f5637cbaddf
soni2261 <61927635+soni2261@users.noreply.github.com>
2024-04-05 23:32:36 -0400

    email verif page with endpoints

--- frontend/components/auth/VerifyEmail.tsx
+++ frontend/components/auth/VerifyEmail.tsx
@@ -2,30 +2,24 @@ import React, { useState } from "react";
 import { Button, FormControl, Input, Stack, Text, useToast, Container } from "@chakra-ui/react";
 import AuthHelper from "../../helpers/AuthHelper";
 
-const ForgotPassword: React.FC = () => {
-  const [email, setEmail] = useState("");
+const VerifyEmail: React.FC = () => {
+  const [token, setEmail] = useState("");
   const toast = useToast();
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
-      const response = await AuthHelper.forgotPassword(email);
+      const response = await AuthHelper.verifyEmail(token);
     
       if (response && response.status === 200) {
-        const message = response.data || "We've sent a password reset link to your email address.";
-        toast({
-          title: "Reset Link Sent",
-          description: message,
-          status: "success",
-          duration: 9000,
-          isClosable: true,
-        });
+        
+      
       } else {
-        throw new Error("Failed to send reset link.");
+        throw new Error("Failed to receive email token");
       }
     } catch (error) {
-      let errorMessage = "The email is not associated to a user.";
+      let errorMessage = "The token is not associated to a user.";
       if (error.response) {
         errorMessage = error.response.data || errorMessage;
       }
@@ -44,24 +38,11 @@ const ForgotPassword: React.FC = () => {
     <>
       <Container variant={"authBox"}>
         <Text fontSize="xl" fontWeight="bold" color="white" mb={4}>
-          Forgot your password?
+          Thank you for verifying your email!
         </Text>
-        <Text fontSize="md" color="gray.400" mb={8}>
-          Enter your email address below and we'll send you a link to reset your password.
-        </Text>
-        <form onSubmit={handleSubmit}>
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" borderRadius="2xl" bg="whiteAlpha.200" borderColor="whiteAlpha.400" _placeholder={{ color: "whiteAlpha.700" }} required />
-            </FormControl>
-            <Button type="submit" bg="brand.100" color="white" size="md" fontSize="md" borderRadius="2xl" py={3} width="full" boxShadow="0 2px 4px rgba(0, 0, 0, 0.2)">
-              Send Reset Link
-            </Button>
-          </Stack>
-        </form>
       </Container>
     </>
   );
 };
 
-export default ForgotPassword;
+export default VerifyEmail;
