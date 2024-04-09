5c53b83933663adbe25e50cdb661f760525e20d0
Sheikh John <jonathan.githubofficial@gmail.com>
2024-04-08 20:58:11 -0400

    updated the greeting page

--- frontend/pages/Greeting.tsx
+++ frontend/pages/Greeting.tsx
@@ -1,128 +1,85 @@
 // pages/greeting.tsx
-import { Box, Button, Flex, Text, useColorModeValue, Img, keyframes, VStack, useBreakpointValue } from '@chakra-ui/react'
+import { Box, Button, Flex, Text, Img, VStack, useBreakpointValue,  } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import React from 'react'
 import { useRouter } from 'next/router'
-const MotionImage = motion(Img)
-import { FaPlus } from 'react-icons/fa6'
+import { FaPlus, } from 'react-icons/fa6'
 import { ClientOnly } from '../components/client-only'
+import logoGreeting from '../public/svgs/logoGreeting.svg'
 
-const fadeIn = keyframes`
-  from { opacity: 0; }
-  to { opacity: 1; }
-`
+
 
 const Greeting = () => {
-  const bg = useColorModeValue('white', 'az.blackish')
-  const router = useRouter()
   const isMobile = useBreakpointValue({ base: true, md: false })
+  const router = useRouter()
+  const MotionImage = motion(Img)
+const GreenGradient = () => (
+  <Box
+    top={["300px", "400px", "600px"]} 
+    left={["100px", "500px", "1400px"]} 
+    right={0}
+    bottom={0}
+    style={{
+      width: '300px',
+      height: '300px',
+      transform: 'rotate(17deg)',
+      transformOrigin: '0 0',
+      opacity: 0.2,
+      background: 'linear-gradient(180deg, #89DBBD 0%, #FFD569 99%)',
+      boxShadow: '85.9000015258789px 85.9000015258789px 85.9000015258789px',
+      borderRadius: 9999,
+      filter: 'blur(85.90px)',
+      transition: 'all 0.5s ease-in-out',
+    }}
+    zIndex={1}
+  />
+)
 
-  return isMobile ? (
+const RedGradient = () => (
+  <Box
+  top={400}
+  left={700}
+  right={0}
+  bottom={0}
+  style={{
+    width: '195px',
+    height: '195px',
+    transform: 'rotate(17deg)',
+    transformOrigin: '0 0',
+    background: 'linear-gradient(180deg, #FF6B60 21%, #FFD569 100%)',
+    boxShadow: '85.9000015258789px 85.9000015258789px 85.9000015258789px',
+    borderRadius: 9999,
+    filter: 'blur(85.90px)',
+    animation: `rotateBox 2s linear infinite`,
+    transition: 'all 0.5s ease-in-out',
+  }}
+  zIndex={0}
+/>
+)
+  return (
     <ClientOnly>
-      <Box bg={bg} h="100vh" overflow="hidden" px={8} py={12}>
-        {/* Logo awaazologo.png*/}
-        <VStack align="center" justify="flex-end" h="100%" spacing={8}>
-          {/* white box */}
-          <Box
-            bg={`rgba(255, 255, 255, 0.15)`}
-            p={'2.5em'}
-            borderRadius={20}
-            boxShadow="lg"
-            w="full"
-            maxW="md"
-            height={'60%'}
-            position="relative"
-            style={{
-              position: 'relative', // Ensure the plus pattern box is contained within this box
-            }}
-          >
-            {/* Plus icon grid */}
-            <Box
-              position="absolute"
-              top={0}
-              left={0}
-              right={0}
-              bottom={0}
-              display="grid"
-              gridGap="5px"
-              gridTemplateColumns="repeat(auto-fill, 50px)"
-              gridTemplateRows="repeat(auto-fill, 50px)"
-              justifyItems="center"
-              alignItems="center"
-              overflow="hidden"
-              zIndex={0} // Ensure the plus pattern is behind other content
-            >
-              {Array.from({ length: 200 }).map((_, index) => (
-                <FaPlus key={index} size={20} color="#0000002A" />
-              ))}
-            </Box>
-            {/* Bird flying in space */}
-            <MotionImage
-              src="/svgs/waazoFlying.svg"
-              alt="Awaazo Flying"
-              boxSize="250px"
-              position="absolute"
-              left="7.5em"
-              top="-10em"
-              transform="translateX(-50%)"
-              animate={{ rotate: [0, 10, -10, 0], x: [0, 5, -5, 0], y: [0, -5, 5, 0] }}
-              transition={{ duration: 10, ease: 'easeInOut', loop: Infinity, repeatDelay: 1 }}
-            />
-            <VStack align="center" justify="space-between" spacing={8} h="100%">
-              {/* Empty space */}
-              <Box flex="1" />
-              {/* Description */}
-              <VStack spacing={10} justify="flex-end">
-                <Text align="left">
-                  <Text fontSize="2em" opacity={0.5} fontWeight={'bold'}>
-                    Welcome to
-                  </Text>
-                  <Text fontSize="2em" fontWeight={'extrabold'}>
-                    AWAAZO.
-                  </Text>
-                  <br />
-                  <Text fontSize="15px" fontWeight={'extrabold'}>
-                    Awaazo revolutionizes podcasting by blending traditional streaming with advanced AI tools for content creation, discovery, and interaction, featuring live transcripts, a
-                    knowledgeable AI assistant, a diverse library, and a community for creators to share insights.
-                  </Text>
-                </Text>
-                {/* Login and sign up buttons */}
-                <VStack spacing={5} align="stretch">
-                  <Button colorScheme="teal" size="lg" onClick={() => router.push('/auth/Signup')} borderRadius="17px" fontSize={'1em'} boxShadow="md">
-                    GET STARTED
-                  </Button>
-                  <Button colorScheme="orange" size="lg" onClick={() => router.push('/auth/Login')} borderRadius="17px" fontSize={'1em'} boxShadow="md">
-                    I ALREADY HAVE AN ACCOUNT
-                  </Button>
-                </VStack>
-              </VStack>
-            </VStack>
-          </Box>
-          {/* Footer text */}
-          <Text fontSize="xs" position="absolute" bottom="4" left="4">
-            Created by the Awaazo team 2024
-          </Text>
-        </VStack>
-      </Box>
-    </ClientOnly>
-  ) : (
-    <ClientOnly>
-      <Box bg={bg} h="100vh" overflow="hidden" px={8} py={12}>
+      <Box bg={'az.blackish'} h="100vh" overflow="hidden" px={8} py={10}>
         <Flex justify="center" align="center" h="100%">
+          
           <VStack align="center" spacing={8}>
-            <Box
-              bg={`rgba(255, 255, 255, 0.15)`}
-              p={'2.5em'}
-              borderRadius={20}
-              boxShadow="lg"
-              w="full"
-              maxW="xl"
-              height={'60%'}
-              position="relative"
-              style={{
-                position: 'relative', // Ensure the plus pattern box is contained within this box
-              }}
-            >
+          <Img src={logoGreeting.src} alt="Logo Greeting" />
+           
+          <RedGradient/>
+            <GreenGradient/>
+            <Box bg={`az.darkGrey`} p={'2.5em'} borderRadius={'20px'} boxShadow="lg" w="full" maxW="xl" height={'60%'} position="relative">
+              <MotionImage
+                src="/svgs/waazoFlying.svg"
+                alt="Awaazo Flying"
+                boxSize="300px"
+                position="absolute"
+                left="12em"
+                top="-12em"
+                transform="translateX(-50%)"
+                animate={{ rotate: [0, 10, -10, 0], x: [0, 5, -5, 0], y: [0, -5, 5, 0] }}
+                transition={{ duration: 10, ease: 'easeInOut', loop: Infinity, repeatDelay: 1 }}
+                zIndex={2}
+              />
+
               <Box
                 position="absolute"
                 top={0}
@@ -130,58 +87,65 @@ const Greeting = () => {
                 right={0}
                 bottom={0}
                 display="grid"
-                gridGap="5px"
-                gridTemplateColumns="repeat(auto-fill, 50px)"
-                gridTemplateRows="repeat(auto-fill, 50px)"
+                gridGap="2px"
+                gridTemplateColumns="repeat(auto-fill, 30px)"
+                gridTemplateRows="repeat(auto-fill, 30px)"
                 justifyItems="center"
                 alignItems="center"
                 overflow="hidden"
-                zIndex={0}
+                zIndex={1}
               >
-                {Array.from({ length: 200 }).map((_, index) => (
-                  <FaPlus key={index} size={20} color="#0000002A" />
+                {Array.from({ length: 300 }).map((_, index) => (
+                  <FaPlus key={index} size={20} color="#2D2D2D" />
                 ))}
               </Box>
-              <MotionImage
-                src="/svgs/waazoFlying.svg"
-                alt="Awaazo Flying"
-                boxSize="250px"
-                position="absolute"
-                left="11em"
-                top="-10em"
-                transform="translateX(-50%)"
-                animate={{ rotate: [0, 10, -10, 0], x: [0, 5, -5, 0], y: [0, -5, 5, 0] }}
-                transition={{ duration: 10, ease: 'easeInOut', loop: Infinity, repeatDelay: 1 }}
-              />
 
               <VStack align="center" justify="space-between" spacing={8} h="100%">
-                <Box flex="1" />
-                <VStack spacing={10} justify="flex-end">
-                  <Text align="left">
-                    <Text fontSize="2em" opacity={0.5} fontWeight={'bold'}>
+                <Box zIndex={3}>
+                  <VStack align="left" spacing={0}>
+                    <Text fontSize="lg" opacity={0.5} fontWeight={'bold'} color={'az.greyish'}>
                       Welcome to
                     </Text>
-                    <Text fontSize="2em" fontWeight={'extrabold'}>
+                    <Text fontSize="xxl" fontWeight={'bold'} mt={-3}>
                       AWAAZO.
                     </Text>
-                    <br />
-                    <Text fontSize="15px" fontWeight={'extrabold'}>
+                    <Text fontSize="sm" fontWeight={'normal'} color={'az.greyish'} mt={-1}>
                       Awaazo revolutionizes podcasting by blending traditional streaming with advanced AI tools for content creation, discovery, and interaction, featuring live transcripts, a
                       knowledgeable AI assistant, a diverse library, and a community for creators to share insights.
                     </Text>
-                  </Text>
-                  <VStack spacing={5} align="stretch">
-                    <Button colorScheme="teal" size="lg" onClick={() => router.push('/auth/Signup')} borderRadius="17px" fontSize={'1em'} boxShadow="md">
+                  </VStack>
+
+                  <VStack spacing={5} align="stretch" mt="1.5rem">
+                    <Button
+                      onClick={() => router.push('/auth/Signup')}
+                      variant="large"
+                      background="az.green"
+                      _hover={{
+                        background: '#AAE5CF',
+                        color: 'white',
+                      }}
+                      transition="all 0.3s ease-in-out"
+                    >
                       GET STARTED
                     </Button>
-                    <Button colorScheme="orange" size="lg" onClick={() => router.push('/auth/Login')} borderRadius="17px" fontSize={'1em'} boxShadow="md">
+                    <Button
+                      onClick={() => router.push('/auth/Login')}
+                      variant="large"
+                      background="az.yellow"
+                      _hover={{
+                        background: '#FFE193',
+                        color: 'white',
+                      }}
+                      transition="all 0.3s ease-in-out"
+                    >
                       I ALREADY HAVE AN ACCOUNT
                     </Button>
                   </VStack>
-                </VStack>
+                </Box>
               </VStack>
             </Box>
-            <Text fontSize="xs">Created by the Awaazo team 2024</Text>
+
+            <Text fontSize="xs"> © Created by the Awaazo team 2024. </Text>
           </VStack>
         </Flex>
       </Box>
--- frontend/styles/AppTheme.tsx
+++ frontend/styles/AppTheme.tsx
@@ -98,6 +98,7 @@ const overrides = {
             color: 'white', 
           },
         },
+
         normal: {
           display: 'flex',
           height: '37px',
